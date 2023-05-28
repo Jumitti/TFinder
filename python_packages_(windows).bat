d8e35ab2084163e33893965a2e44a30e5befee48
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-05-28 22:40:12 +0200

    Add files via upload

---
+++ python_packages_(windows).bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM Installation des packages Python
+pip install requests
+pip install pillow
+pip install pyperclip
+pip install tabulate
+
+REM Fin de l'installation
+echo Packages Python installés avec succès.
+pause
