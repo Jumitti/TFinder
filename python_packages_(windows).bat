08570ed207b2aa5d2c81ea622fbeeac7d86351ba
Minniti Julien <112354223+Jumitti@users.noreply.github.com>
2023-05-29 12:04:55 +0200

    Update python_packages_(windows).bat

--- python_packages_(windows).bat
+++ python_packages_(windows).bat
@@ -1,10 +1,11 @@
 @echo off
 
 REM Installation of python packages
-pip install requests
 pip install pillow
 pip install pyperclip
+pip install requests
 pip install tabulate
+pip install tk
 
 REM End of installation
 echo Python packages successfully installed.
