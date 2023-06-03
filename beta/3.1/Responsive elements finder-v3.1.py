9268ecf8b8c68adc1717d84ff23c2af158c0f0c5
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-03 20:53:38 +0200

    Near to release

--- beta/3.1/Responsive elements finder-v3.1.py
+++ beta/3.1/Responsive elements finder-v3.1.py
@@ -12,13 +12,15 @@ from PIL import Image, ImageTk
 from tabulate import tabulate
 from tkinter import filedialog
 
+#Promoter Finder
+
 # Convert gene to ENTREZ_GENE_ID
 def convert_gene_names_to_entrez_ids(gene_names):
     try:
         entrez_ids = []
         for gene_id in gene_names:
             species = species_combobox.get()
-            # Requête de recherche de gène par nom et espèce
+            # Request for ENTREZ_GENE_ID
             url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term={gene_id}[Gene%20Name]+AND+{species}[Organism]&retmode=json&rettype=xml"
             response = requests.get(url)
 
@@ -106,18 +108,6 @@ def get_dna_sequence(chraccver, chrstart, chrstop, upstream, downstream):
     except Exception as e:
         raise Exception(f"Error : {str(e)}")
 
-# Copy/Paste Button
-def copy_sequence():
-    sequence = result_text.get('1.0', tk.END).strip()
-    pyperclip.copy(sequence)
-    messagebox.showinfo("Copy", "The sequence has been copied to the clipboard.")
-
-def paste_sequence():
-    sequence = window.clipboard_get()
-    text_promoter.delete("1.0", "end")
-    text_promoter.insert("1.0", sequence)
-    messagebox.showinfo("Paste", "The sequence has been pasted.")
-
 # Display gene and promoter
 def get_sequence():
     species = species_combobox.get()
@@ -125,14 +115,14 @@ def get_sequence():
     total_gene_ids = len(gene_ids)
     upstream = int(upstream_entry.get())
     downstream = int(downstream_entry.get())
-    result_text.delete("1.0", tk.END)
+    result_promoter.delete("1.0", tk.END)
     
     for i, gene_id in enumerate(gene_ids, start=1):
         try:
             number_gene_id = i
             
-            # Convertir le nom du gène en ENTREZ_GENE_ID si nécessaire
-            gene_names = []  # Déclaration de gene_names
+            # gene name to ENTREZ_GENE_ID
+            gene_names = []
             if not gene_id.isdigit():
                 gene_id = gene_id.strip("'\'")
                 gene_names.append(gene_id)
@@ -144,7 +134,7 @@ def get_sequence():
                 gene_entrez_id = [gene_id]
             
         except Exception as e:
-            result_text.insert(tk.END, f"Error retrieving gene information for ID: {gene_id}\nError: {str(e)}\n")
+            result_promoter.insert(tk.END, f"Error retrieving gene information for ID: {gene_id}\nError: {str(e)}\n")
                 
         # Gene information retrieval
         for gene_id in gene_entrez_id:
@@ -170,26 +160,17 @@ def get_sequence():
             text_status.insert("1.0", f"Extract {gene_name} promoter -> Done ({number_gene_id}/{total_gene_ids})")
             window.update_idletasks()
 
-            # Append the result to the result_text
-            result_text.insert(tk.END, f">{gene_name} | {species} | {chraccver} | TSS: {chrstart}\n{dna_sequence}\n\n")
-            text_status.delete("1.0", "end")
-            text_status.insert("1.0", f"Extract promoter -> Done ({number_gene_id}/{total_gene_ids})")
+            # Append the result to the result_promoter
+            result_promoter.insert(tk.END, f">{gene_name} | {species} | {chraccver} | TSS: {chrstart}\n{dna_sequence}\n\n")
             window.update_idletasks()
+    
+    text_status.delete("1.0", "end")
+    text_status.insert("1.0", f"Extract promoter -> Done ({number_gene_id}/{total_gene_ids})")
+    global entry_tis_var
+    entry_tis_var.set(upstream)
+    messagebox.showinfo("Promoter", "Promoters region extracted.")
 
-# Reverse complement
-def reverse_complement(sequence):
-    text_status.delete("1.0", "end")
-    status = "Reverse complement..."
-    text_status.insert("1.0", status)
-    window.update_idletasks()
-    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    reverse_sequence = sequence[::-1]
-    complement_sequence = ''.join(complement_dict.get(base, base) for base in reverse_sequence)
-    text_status.delete("1.0", "end")
-    status = "Reverse complement -> Done"
-    text_status.insert("1.0", status)
-    window.update_idletasks()
-    return complement_sequence
+# Responsive-Elements-Finder
 
 # Generation of all responsive elements
 def generate_variants(sequence):
@@ -271,7 +252,7 @@ def find_sequence_consensus():
     threshold = float(threshold_entry.get())
 
     # Promoter input type
-    lines = text_promoter.get("1.0", "end-1c")
+    lines = result_promoter.get("1.0", "end-1c")
     promoters = []
     
     first_line = lines
@@ -280,7 +261,7 @@ def find_sequence_consensus():
         promoter_region = lines
         promoters.append((shortened_promoter_name, promoter_region))
     else :
-        lines = text_promoter.get("1.0", "end-1c").split("\n")
+        lines = result_promoter.get("1.0", "end-1c").split("\n")
         i = 0
         while i < len(lines):
             line = lines[i]
@@ -293,11 +274,10 @@ def find_sequence_consensus():
             else:
                 i += 1
 
-    # Affichage des noms et des séquences correspondantes
+    # REF
     for shortened_promoter_name, promoter_region in promoters:
         found_positions = []
         
-        # Définir le motif cyclique de caractères
         pattern = "\|/-\|/-"
         cycle = itertools.cycle(pattern)
         
@@ -381,6 +361,39 @@ def find_sequence_consensus():
 table = []
 header = ["Position", "Position (TIS)", "Sequence", "% Homology", "Ref seq", "Prom."]
 
+# Common functions
+
+# Reverse complement
+def reverse_complement(sequence):
+    text_status.delete("1.0", "end")
+    status = "Reverse complement..."
+    text_status.insert("1.0", status)
+    window.update_idletasks()
+    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    reverse_sequence = sequence[::-1]
+    complement_sequence = ''.join(complement_dict.get(base, base) for base in reverse_sequence)
+    text_status.delete("1.0", "end")
+    status = "Reverse complement -> Done"
+    text_status.insert("1.0", status)
+    window.update_idletasks()
+    return complement_sequence
+
+# Copy/Paste Button
+def copy_sequence():
+    sequence = result_promoter.get('1.0', tk.END).strip()
+    pyperclip.copy(sequence)
+    messagebox.showinfo("Copy", "The sequence has been copied to the clipboard.")
+
+def paste_sequence():
+    sequence = window.clipboard_get()
+    result_promoter.delete("1.0", "end")
+    result_promoter.insert("1.0", sequence)
+    
+def paste_gene():
+    gene = window.clipboard_get()
+    gene_id_entry.delete("1.0", "end")
+    gene_id_entry.insert("1.0", gene)
+
 #Export to excel
 def export_to_excel():
     file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel Files", "*.xlsx")])
@@ -401,124 +414,135 @@ def show_help_PDF():
     pdf_path = os.path.join(script_directory, "Promoter_finder_HELP.pdf")
     webbrowser.open(pdf_path)
 
-#Logo
-script_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(script_dir, "REF.png")
-
 # Github
 def open_site():
     url = "https://github.com/Jumitti/Responsive-Elements-Finder"
     webbrowser.open(url)
-    
+
+# TK window
+
+#Logo
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, "REF.png")
+
 # Create TK windows
 window = tk.Tk()
 window.title("Responsive Elements Finder")
 logo_image = tk.PhotoImage(file=image_path)
 window.iconphoto(True, logo_image)
 
-#How to use
-help_button = tk.Button(window, text="How to use", command=show_help_PDF)
-help_button.place(x=10, y=10)
-
-# Github
-button = tk.Button(window, text="Github by MINNITI Julien", command=open_site)
-button.place(x=10, y=40)
-
 # Section "Promoter finder"
 section_promoter_finder = tk.LabelFrame(window, text="Promoter Finder")
-section_promoter_finder.grid(row=0, column=1, padx=10, pady=10)
+section_promoter_finder.grid(row=0, column=0, padx=10, pady=10)
 
 # Gene ID entry
 gene_id_label = tk.Label(section_promoter_finder, text="Gene ID:")
 gene_id_label.grid(row=0, column=0)
 gene_id_entry = tk.Text(section_promoter_finder, height=5, width=20)
 gene_id_entry.grid(row=1, column=0)
+paste_button = tk.Button(section_promoter_finder, text="Paste", command=paste_gene)
+paste_button.grid(row=2, column=0)
 
 
 # Species selection
 species_label = tk.Label(section_promoter_finder, text="Species:")
-species_label.grid(row=2, column=0)
+species_label.grid(row=3, column=0)
 species_combobox = ttk.Combobox(section_promoter_finder, values=["Human", "Mouse", "Rat"])
-species_combobox.grid(row=3, column=0)
+species_combobox.current(0)
+species_combobox.grid(row=4, column=0)
 
 # Upstream/downstream entry
 upstream_label = tk.Label(section_promoter_finder, text="Upstream (bp):")
-upstream_label.grid(row=4, column=0)
+upstream_label.grid(row=5, column=0)
 upstream_entry = tk.Entry(section_promoter_finder)
 upstream_entry.insert(2000, "2000")  # $"2000" default
-upstream_entry.grid(row=5, column=0)
+upstream_entry.grid(row=6, column=0)
 
 downstream_label = tk.Label(section_promoter_finder, text="Downstream (bp):")
-downstream_label.grid(row=6, column=0)
+downstream_label.grid(row=7, column=0)
 downstream_entry = tk.Entry(section_promoter_finder)
 downstream_entry.insert(500, "500")  # $"500" default
-downstream_entry.grid(row=7, column=0)
+downstream_entry.grid(row=8, column=0)
 
 # Search
 search_button = tk.Button(section_promoter_finder, text="Find promoter  (CAN BE STUCK ! Don't worry, just wait ~30sec/gene)", command=get_sequence)
-search_button.grid(row=8, column=0)
+search_button.grid(row=9, column=0)
+
+# Section "Promoter"
+section_promoter = tk.LabelFrame(section_promoter_finder, text="Promoter Finder")
+section_promoter.grid(row=10, column=0, padx=10, pady=10)
 
 # Promoter output
-result_text = tk.Text(section_promoter_finder, height=10, width=50)
-result_text.grid(row=9, column=0)
-copy_button = tk.Button(section_promoter_finder, text="Copy", command=copy_sequence)
-copy_button.grid(row=10, column=0)
+result_promoter = tk.Text(section_promoter, height=10, width=50)
+result_promoter.pack()
+copy_button = tk.Button(section_promoter, text="Copy", command=copy_sequence)
+copy_button.pack(side="left", fill="x", expand=True)
+paste_button = tk.Button(section_promoter, text="Paste", command=paste_sequence)
+paste_button.pack(side="left", fill="x", expand=True)
+
+# Section help and GitHub
+
+# Help & Credit
+section_help = tk.LabelFrame(window, text="Help & Credit")
+section_help.grid(row=1, column=0, padx=10, pady=10)
+
+#How to use
+help_button = tk.Button(section_help, text="How to use", command=show_help_PDF)
+help_button.pack(side="left", fill="x", expand=True)
+
+# Github
+button = tk.Button(section_help, text="Github by MINNITI Julien", command=open_site)
+button.pack(side="left", fill="x", expand=True)
 
 # Section "Responsive Elements finder"
 section_responsive_finder = tk.LabelFrame(window, text="Responsive Elements Finder")
-section_responsive_finder.grid(row=0, column=2, padx=10, pady=10)
-
-# Promoter entry
-label_promoter = tk.Label(section_responsive_finder, text="Promoter")
-label_promoter.grid(row=0, column=0)
-text_promoter = tk.Text(section_responsive_finder, height=2, width=50)
-text_promoter.grid(row=1, column=0)
-paste_button = tk.Button(section_responsive_finder, text="Past", command=paste_sequence)
-paste_button.grid(row=2, column=0)
+section_responsive_finder.grid(row=0, column=1, padx=10, pady=10)
 
 # RE entry
 label_sequence = tk.Label(section_responsive_finder, text="Responsive element (IUPAC authorize) :")
-label_sequence.grid(row=4, column=0)
+label_sequence.grid(row=1, column=0)
 entry_sequence = tk.Entry(section_responsive_finder)
-entry_sequence.grid(row=5, column=0)
+entry_sequence.insert(0, "ATGCCGTA")  # default
+entry_sequence.grid(row=2, column=0)
 
 # TIS entry
 label_tis = tk.Label(section_responsive_finder, text="Transcription Initiation Site (bp)")
-label_tis.grid(row=6, column=0)
+label_tis.grid(row=3, column=0)
 label_tis_info = tk.Label(section_responsive_finder, text="(distance from start of promoter or 'Upstream' if you use the Promoter Finder)")
-label_tis_info.grid(row=7, column=0)
-entry_tis = tk.Entry(section_responsive_finder)
-entry_tis.insert(0, "0")  # $"0" default
-entry_tis.grid(row=8, column=0)
+label_tis_info.grid(row=4, column=0)
+entry_tis_var = tk.StringVar()
+entry_tis_var.set("0")  # Valeur par défaut
+entry_tis = tk.Entry(section_responsive_finder, textvariable=entry_tis_var)
+entry_tis.grid(row=5, column=0)
 
 # Threshold
 threshold_label = tk.Label(section_responsive_finder, text="Threshold (%)")
-threshold_label.grid(row=9, column=0)
+threshold_label.grid(row=6, column=0)
 threshold_entry = tk.Entry(section_responsive_finder)
 threshold_entry.insert(80, "80")  # $"80" default
-threshold_entry.grid(row=10, column=0)
+threshold_entry.grid(row=7, column=0)
 
 # Find RE
 button_search = tk.Button(section_responsive_finder, text="Find responsive elements", command=find_sequence_consensus)
-button_search.grid(row=11, column=0)
+button_search.grid(row=8, column=0)
 
 # RE output
 label_result = tk.Label(section_responsive_finder, text="Responsive elements")
-label_result.grid(row=12, column=0)
+label_result.grid(row=9, column=0)
 text_result = tk.Text(section_responsive_finder, height=16, width=100)
-text_result.grid(row=13, column=0)
+text_result.grid(row=10, column=0)
 
 # Création du bouton Export to Excel
 export_button = tk.Button(section_responsive_finder, text="Export to Excel", command=export_to_excel)
-export_button.grid(row=14, column=0)
+export_button.grid(row=11, column=0)
 
 # Section "status"
-section_status = tk.LabelFrame(window, text="status")
-section_status.grid(row=15, column=2, padx=10, pady=10)
+section_status = tk.LabelFrame(window, text="Status")
+section_status.grid(row=1, column=1, padx=10, pady=10)
 
 # status output
 text_status = tk.Text(section_status, height=1, width=100)
-text_status.grid(row=16, column=0)
+text_status.grid(row=0, column=0)
 
 # Configure grid weights
 window.grid_rowconfigure(0, weight=1)
