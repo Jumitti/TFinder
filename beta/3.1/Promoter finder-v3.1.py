88c12dfb43fd0280fd91135f0facc645373c3f17
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-03 08:35:58 +0200

    Gene input to ENTREZ_GENE_ID
    If gene input is not ENTREZ_GENE_ID, they are converted

--- beta/3.1/Promoter finder-v3.1.py
+++ beta/3.1/Promoter finder-v3.1.py
@@ -11,6 +11,32 @@ from PIL import Image, ImageTk
 from tabulate import tabulate
 from tkinter import filedialog
 
+# Convert gene to ENTREZ_GENE_ID
+def convert_gene_names_to_entrez_ids(gene_names):
+    try:
+        entrez_ids = []
+        for gene_name in gene_names:
+            # Requête de recherche de gène par nom et espèce
+            url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term={gene_name}[Gene%20Name]+AND+{species}[Organism]"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                response_data = response.json()
+
+                if response_data['eSearchResult']['Count'] == '0':
+                    raise Exception(f"No gene found for name: {gene_name}")
+
+                entrez_id = response_data['eSearchResult']['IdList']['Id'][1]
+                entrez_ids.append(entrez_id)
+
+            else:
+                raise Exception(f"Error during gene search: {response.status_code}")
+
+        return entrez_ids
+
+    except Exception as e:
+        raise Exception(f"Error: {str(e)}")
+
 #Gene informations
 def get_gene_info(gene_id, species):
     try:
@@ -91,16 +117,26 @@ def paste_sequence():
 
 # Display gene and promoter
 def get_sequence():
+    species = species_combobox.get()
     gene_ids = gene_id_entry.get("1.0", tk.END).strip().split("\n")
     total_gene_ids = len(gene_ids)
-    species = species_combobox.get()
     upstream = int(upstream_entry.get())
     downstream = int(downstream_entry.get())
     result_text.delete("1.0", tk.END)
     for i, gene_id in enumerate(gene_ids, start=1):
         try:
             number_gene_id = i
-            
+
+            # Convertir le nom du gène en ENTREZ_GENE_ID si nécessaire
+            if not gene_id.isdigit():
+                gene_names = gene_id.strip("'\"")
+                print(gene_names)
+                gene_ids = convert_gene_names_to_entrez_ids(gene_names)
+                gene_id = gene_ids
+                print(gene_id)
+            else :
+                gene_id = gene_id.strip("'\"")
+
             # Gene information retrieval
             text_statut.delete("1.0", "end")
             text_statut.insert("1.0", f"Find gene information... ({number_gene_id}/{total_gene_ids})")
@@ -110,7 +146,7 @@ def get_sequence():
             text_statut.delete("1.0", "end")
             text_statut.insert("1.0", f"Find {gene_name} information -> Done ({number_gene_id}/{total_gene_ids})")
             window.update_idletasks()
-            
+
             chraccver = gene_info['genomicinfo'][0]['chraccver']
             chrstart = gene_info['genomicinfo'][0]['chrstart']
             chrstop = gene_info['genomicinfo'][0]['chrstop']
@@ -123,7 +159,7 @@ def get_sequence():
             text_statut.delete("1.0", "end")
             text_statut.insert("1.0", f"Extract {gene_name} promoter -> Done ({number_gene_id}/{total_gene_ids})")
             window.update_idletasks()
-            
+
             # Append the result to the result_text
             result_text.insert(tk.END, f">{gene_name} | {chraccver} | TIS: {chrstart}\n{dna_sequence}\n\n")
             text_statut.delete("1.0", "end")
@@ -147,6 +183,22 @@ def reverse_complement(sequence):
     window.update_idletasks()
     return complement_sequence
 
+# Convert gene names to ENTREZ_GENE_ID
+def convert_gene_names_to_entrez_ids(gene_names):
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    url = f"{base_url}esearch.fcgi?db=gene&term="
+    for name in gene_names:
+        url += f"{name}[gene]+OR+"
+    url = url[:-4]
+    url += "&retmode=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_data = response.json()
+        id_list = response_data["esearchresult"]["idlist"]
+        return id_list
+    else:
+        raise Exception(f"Error during conversion of gene names to ENTREZ_GENE_ID : {response.status_code}")
+
 #HELP
 def show_help_PDF():
     script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -161,7 +213,7 @@ image_path = os.path.join(script_dir, "REF.png")
 def open_site():
     url = "https://github.com/Jumitti/Responsive-Elements-Finder"
     webbrowser.open(url)
-    
+
 # Create TK windows
 window = tk.Tk()
 window.title("Promoter finder")
@@ -195,48 +247,61 @@ gene_id_entry.grid(row=1, column=0)
 species_label = tk.Label(section_promoter_finder, text="Species:")
 species_label.grid(row=2, column=0)
 species_combobox = ttk.Combobox(section_promoter_finder, values=["Human", "Mouse", "Rat"])
+species_combobox.current(0)
 species_combobox.grid(row=3, column=0)
 
-# Upstream/downstream entry
-upstream_label = tk.Label(section_promoter_finder, text="Upstream (bp):")
-upstream_label.grid(row=4, column=0)
-upstream_entry = tk.Entry(section_promoter_finder)
-upstream_entry.insert(2000, "2000")  # $"2000" default
-upstream_entry.grid(row=5, column=0)
+# Upstream distance entry
+upstream_label = tk.Label(section_promoter_finder, text="Upstream Distance:")
+upstream_label.grid(row=0, column=1)
+upstream_entry = tk.Entry(section_promoter_finder, width=10)
+upstream_entry.grid(row=1, column=1)
+upstream_entry.insert(tk.END, "2000")
 
-downstream_label = tk.Label(section_promoter_finder, text="Downstream (bp):")
-downstream_label.grid(row=6, column=0)
-downstream_entry = tk.Entry(section_promoter_finder)
-downstream_entry.insert(500, "500")  # $"500" default
-downstream_entry.grid(row=7, column=0)
+# Downstream distance entry
+downstream_label = tk.Label(section_promoter_finder, text="Downstream Distance:")
+downstream_label.grid(row=2, column=1)
+downstream_entry = tk.Entry(section_promoter_finder, width=10)
+downstream_entry.grid(row=3, column=1)
+downstream_entry.insert(tk.END, "200")
 
-# Search
-search_button = tk.Button(section_promoter_finder, text="Find promoter  (CAN BE STUCK ! Don't worry, just wait)", command=get_sequence)
-search_button.grid(row=8, column=0)
+# Promoter Finder button
+find_button = tk.Button(section_promoter_finder, text="Find Promoters", command=get_sequence)
+find_button.grid(row=4, column=0, columnspan=2, pady=10)
 
-# Section "Results"
-section_results = tk.LabelFrame(window, text="Results")
-section_results.grid(row=15, column=1, padx=10, pady=10)
+# Copy/Paste section
+copy_paste_frame = tk.Frame(window)
+copy_paste_frame.grid(row=1, column=1, padx=10)
 
-# Results output
-result_text = tk.Text(section_results, height=10, width=50)
-result_text.grid(row=0, column=0)
-copy_button = tk.Button(section_results, text="Copy", command=copy_sequence)
-copy_button.grid(row=1, column=0)
+# Copy Button
+copy_button = tk.Button(copy_paste_frame, text="Copy", command=copy_sequence)
+copy_button.grid(row=0, column=0, padx=5)
 
-# Section "Statut"
-section_statut = tk.LabelFrame(window, text="Statut")
-section_statut.grid(row=16, column=1, padx=10, pady=10)
+# Paste Button
+paste_button = tk.Button(copy_paste_frame, text="Paste", command=paste_sequence)
+paste_button.grid(row=0, column=1, padx=5)
 
-# Statut output
-text_statut = tk.Text(section_statut, height=1, width=50)
-text_statut.grid(row=0, column=0)
+# Result section
+result_frame = tk.LabelFrame(window, text="Promoter Sequences")
+result_frame.grid(row=0, column=2, rowspan=2, padx=10, pady=10)
 
-# Configure grid weights
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=1)
-window.grid_columnconfigure(2, weight=1)
-section_promoter_finder.grid_rowconfigure(13, weight=1)
+# Result text
+result_text = tk.Text(result_frame, height=20, width=50)
+result_text.pack(side=tk.LEFT, fill=tk.BOTH)
+result_scroll = tk.Scrollbar(result_frame, orient=tk.VERTICAL)
+result_scroll.config(command=result_text.yview)
+result_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+result_text.config(yscrollcommand=result_scroll.set)
 
-# Lancement de la boucle principale
+# Status section
+status_frame = tk.LabelFrame(window, text="Status")
+status_frame.grid(row=2, column=1, columnspan=2, padx=10, pady=10)
+
+# Status text
+text_statut = tk.Text(status_frame, height=2, width=50)
+text_statut.pack(side=tk.LEFT, fill=tk.BOTH)
+scrollbar_statut = tk.Scrollbar(status_frame, orient=tk.VERTICAL)
+scrollbar_statut.config(command=text_statut.yview)
+scrollbar_statut.pack(side=tk.RIGHT, fill=tk.Y)
+text_statut.config(yscrollcommand=scrollbar_statut.set)
+
 window.mainloop()
