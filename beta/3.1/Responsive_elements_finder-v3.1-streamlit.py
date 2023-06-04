f31751097e24d0b2db4070d0ebecb8bb44462384
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-04 22:03:46 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -175,7 +175,7 @@ if st.button("Find promoter (~30sec/gene)"):
     st.success("Promoters extraction complete!")
 
 #Promoter
-if result_promoter is not None:
+if result_promoter is None:
     st.text_area("Promoter:")
     st.text("Copy: CTRL+A CTRL+C") 
 else:
