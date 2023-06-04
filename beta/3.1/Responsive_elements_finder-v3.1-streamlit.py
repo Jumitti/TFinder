1b706dcd9d3396b5a2dc1045125a3c94e777eae7
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-04 21:04:41 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -12,8 +12,13 @@ st.header('Promoter Finder')
 gene_id_entry = st.text_area("Gene ID:")
 
 # Species
-#st.text("Species :")
-species_combobox = st.selectbox("Species", ["Human", "Mouse", "Rat"], index=0)
+species_combobox = st.selectbox("Species:", ["Human", "Mouse", "Rat"], index=0)
+
+#Upstream
+
+upstream_entry = st.text_input("Upstream:", value="2000")
+
+#Downstream
 
 # Responsive ELements Finder
 st.header('Responsive Elements Finder')
