ec94fceff6e477206ab99c80e96bdef8bb0affa7
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-04 20:34:18 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pyperclip
 
 #StreamLit
 
 st.title('Responsive Elements Finder')
 
+# Promoter Finder STREAMLIT
 st.header('Promoter Finder')
+
+# Gene ID
+if st.button("Paste"):
+    clipboard_content = pyperclip.paste()
+
+    gene_id_entry = st.text_area("Gene ID:", value=clipboard_content)
+
+# Responsive ELements Finder
+st.header('Responsive Elements Finder')
