cc907f434b24a0a640d37ee084df105b1fd953bf
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-04 20:43:55 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -5,13 +5,20 @@ import streamlit as st
 st.title('Responsive Elements Finder')
 
 # Promoter Finder STREAMLIT
+
 st.header('Promoter Finder')
 
 # Gene ID
+gene_id_entry = st.text_area("Gene ID:")
+
 if st.button("Paste"):
     clipboard_content = st.paste()
 
     gene_id_entry = st.text_area("Gene ID:", value=clipboard_content)
+
+# Species
+
+
 
 # Responsive ELements Finder
 st.header('Responsive Elements Finder')
