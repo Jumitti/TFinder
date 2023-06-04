c65e9e0fa0fecfad8a4c9f8eecc2bf2485c6b686
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-05 01:43:05 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -146,12 +146,11 @@ if st.button("Find promoter (~5sec/gene)"):
             st.error(f"Error finding promoters: {str(e)}")
 
 # Promoter output
-if 'result_promoter' in locals():
+if 'result_promoter' not in st.session_state:
     result_promoter_text = "\n".join(result_promoter)
-    if 'result_promoter' not in st.session_state:
-        st.session_state.result_promoter = result_promoter_text
-        result_promoter = st.text_area("Promoter:", value=st.session_state.result_promoter)
-        st.text("Copy: CTRL+A CTRL+C")
+    st.session_state.result_promoter = result_promoter_text
+    result_promoter = st.text_area("Promoter:", value=st.session_state.result_promoter)
+    st.text("Copy: CTRL+A CTRL+C")
 else:
     result_promoter = st.text_area("Promoter:", value="")
 
