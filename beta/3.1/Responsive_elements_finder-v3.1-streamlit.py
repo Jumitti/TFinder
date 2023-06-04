88b31828bb23dbb40f7ba6ee4733f08bd76aea9e
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-04 20:22:47 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -4,50 +4,54 @@ import streamlit as st
 
 st.title('Responsive Elements Finder')
 
+import streamlit as st
+
+# Configure grid layout
+col1, col2 = st.beta_columns(2)
+
 # Section "Promoter finder"
-st.sidebar.markdown("# Promoter Finder")
+with col1:
+    st.markdown("# Promoter Finder")
 
-# Gene ID entry
-gene_id = st.sidebar.text_area("Gene ID:")
+    # Gene ID entry
+    gene_id = st.text_area("Gene ID:")
 
-# Species selection
-species = st.sidebar.selectbox("Species:", ["Human", "Mouse", "Rat"])
+    # Species selection
+    species = st.selectbox("Species:", ["Human", "Mouse", "Rat"])
 
-# Upstream/downstream entry
-upstream = st.sidebar.number_input("Upstream (bp):", value=2000)
-downstream = st.sidebar.number_input("Downstream (bp):", value=500)
+    # Upstream/downstream entry
+    upstream = st.number_input("Upstream (bp):", value=2000)
+    downstream = st.number_input("Downstream (bp):", value=500)
 
-# Search
-if st.sidebar.button("Find promoter"):
-    # Perform promoter search here
-    pass
+    # Search
+    if st.button("Find promoter"):
+        # Perform promoter search here
+        pass
 
 # Section "Responsive Elements finder"
-st.sidebar.markdown("# Responsive Elements Finder")
+with col2:
+    st.markdown("# Responsive Elements Finder")
 
-# RE entry
-responsive_element = st.sidebar.text_input("Responsive element (IUPAC authorize):", value="ATGCCGTA")
+    # RE entry
+    responsive_element = st.text_input("Responsive element (IUPAC authorize):", value="ATGCCGTA")
 
-# TIS entry
-tis = st.sidebar.number_input("Transcription Initiation Site (bp)", value=0)
-st.sidebar.markdown("(distance from start of promoter or 'Upstream' if you use the Promoter Finder)")
+    # TIS entry
+    tis = st.number_input("Transcription Initiation Site (bp)", value=0)
+    st.markdown("(distance from start of promoter or 'Upstream' if you use the Promoter Finder)")
 
-# Threshold
-threshold = st.sidebar.number_input("Threshold (%)", value=80)
+    # Threshold
+    threshold = st.number_input("Threshold (%)", value=80)
 
-# Find RE
-if st.sidebar.button("Find responsive elements"):
-    # Perform responsive elements search here
-    pass
+    # Find RE
+    if st.button("Find responsive elements"):
+        # Perform responsive elements search here
+        pass
 
 # Section "Status"
-st.sidebar.markdown("# Status")
+st.markdown("# Status")
 
-# Configure grid weights
-st.beta_set_page_config(layout="wide")
-
-# Section "Promoter finder"
-st.subheader("Promoter Finder")
+# Section "Promoter"
+st.subheader("Promoter")
 
 # Gene ID output
 st.text_area("Gene ID:", value=gene_id, height=5)
@@ -60,8 +64,8 @@ col1, col2 = st.beta_columns(2)
 col1.text("Upstream (bp): {}".format(upstream))
 col2.text("Downstream (bp): {}".format(downstream))
 
-# Section "Responsive Elements finder"
-st.subheader("Responsive Elements Finder")
+# Section "Responsive elements"
+st.subheader("Responsive Elements")
 
 # Responsive element output
 st.text("Responsive element (IUPAC authorize): {}".format(responsive_element))
@@ -94,3 +98,4 @@ st.text("Status: ")
 st.sidebar.markdown("# Help & Credit")
 st.sidebar.button("How to use")
 st.sidebar.button("Github by MINNITI Julien")
+
