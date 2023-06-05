8a2a58e3ac18c56077ae0798fac29c344588bbce
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-05 22:15:06 +0200

    Update responsive-elements-finder.py

--- streamlit/responsive-elements-finder.py
+++ streamlit/responsive-elements-finder.py
@@ -353,7 +353,7 @@ else:
     st.text("")
 
 # Help
-st.sidebar.markdown("[Github](https://github.com/Jumitti/Responsive-Elements-Finder")
+st.sidebar.markdown("[Github](https://github.com/Jumitti/Responsive-Elements-Finder)")
 st.sidebar.write("By Minniti Julien")
 st.sidebar.title("Help")
 st.sidebar.divider()
