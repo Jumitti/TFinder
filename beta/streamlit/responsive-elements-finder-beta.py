7fb70bba5c0f8b8edf7fdea3b70cb6ab044c6166
Minniti Julien <minnitijulien06@gmail.com>
2023-06-05 18:00:09 +0200

    Update responsive-elements-finder-beta.py

--- beta/streamlit/responsive-elements-finder-beta.py
+++ beta/streamlit/responsive-elements-finder-beta.py
@@ -344,7 +344,7 @@ if 'table' in locals():
     
     ystart = math.floor(df_sorted['% Homology'].min()) - 10
     
-    chart = alt.Chart(df).mark_circle().encode(x=alt.X('Position (TSS)', axis=alt.Axis(title='Position (bp)')),y=alt.Y('% Homology', axis=alt.Axis(title='Homologie %'), scale=alt.Scale(domain=[ystart, 100])).properties(width=600,height=400)
+    chart = alt.Chart(df).mark_circle().encode(x=alt.X('Position (TSS)', axis=alt.Axis(title='Position (bp)')),y=alt.Y('% Homology', axis=alt.Axis(title='Homologie %'), scale=alt.Scale(domain=[ystart, 100]))).properties(width=600,height=400)
 
     # Afficher le graphique dans Streamlit
     st.altair_chart(chart, use_container_width=True)
