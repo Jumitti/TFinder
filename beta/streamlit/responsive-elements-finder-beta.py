ccccca3163bea20a754c8629ac1c7514039a0164
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-05 19:36:27 +0200

    Update responsive-elements-finder-beta.py

--- beta/streamlit/responsive-elements-finder-beta.py
+++ beta/streamlit/responsive-elements-finder-beta.py
@@ -346,7 +346,7 @@ if 'table' in locals():
 
     color_scale = alt.Scale(domain=df['Prom.'].unique(), range=['red', 'blue', 'green', 'yellow'])
 
-    chart = alt.Chart(df_sorted).mark_circle(sort="ascending").encode(
+    chart = alt.Chart(df_sorted).mark_circle().encode(
         x=alt.X('Position (TSS)', axis=alt.Axis(title='Position (bp)')),
         y=alt.Y('% Homology', axis=alt.Axis(title='Homologie %'), scale=alt.Scale(domain=[ystart, 100]))).properties(width=600, height=400)
     
