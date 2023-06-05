b0271c3751c99cfa8e68dee3ec74d52433742b58
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-05 02:06:06 +0200

    Update Responsive_elements_finder-v3.1-streamlit.py

--- beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
+++ beta/3.1/Responsive_elements_finder-v3.1-streamlit.py
@@ -145,7 +145,7 @@ st.session_state.result_promoter = []
 if 'result_promoter' in locals():
     result_promoter_text = "\n".join(result_promoter)
     result_promoter = st.text_area("Promoter:", value=result_promoter_text)
-    st.session_state.result_promoter = result_promoter
+    st.session_state.result_promoter.append = result_promoter
     st.text("Copy: CTRL+A CTRL+C")
 else:
     result_promoter = st.text_area("Promoter:", value="")
