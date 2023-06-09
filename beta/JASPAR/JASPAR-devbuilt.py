2c9744c3c27930e227cfb48c3bc60edbeda40398
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-09 18:54:48 +0200

    Try Reverse complement finding

--- beta/JASPAR/JASPAR-devbuilt.py
+++ beta/JASPAR/JASPAR-devbuilt.py
@@ -8,7 +8,7 @@ def search_sequence():
     # Récupération des valeurs des champs d'entrée
     jaspar_id = entry_jaspar.get()
     promoter_sequence = entry_promoter.get()
-    
+
     # Appel à l'API de Jaspar pour récupérer la matrice de fréquence
     url = f"https://jaspar.genereg.net/api/v1/matrix/{jaspar_id}/"
     response = requests.get(url)
@@ -19,36 +19,53 @@ def search_sequence():
         messagebox.showerror("Erreur", f"Erreur lors de la récupération de la matrice de fréquence : {response.status_code}")
         return
 
-    # Recherche de la séquence référence dans le promoteur
+     # Recherche de la séquence référence dans le promoteur
     results = []
-    seq_length = len(matrix['A'])  # Utiliser la longueur de n'importe quelle clé, car elles doivent toutes avoir la même longueur
-    for i in range(len(promoter_sequence) - seq_length + 1):
-        seq = promoter_sequence[i:i+seq_length]
-        score = calculate_score(seq, matrix)
-        results.append({'Sequence': seq, 'Score': score})
+    seq_length = len(matrix['A'])
 
-    # Affichage des résultats dans un tableau
+    # Calcul du score maximum pour chaque position
+    max_scores = [max(matrix[base][i] for base in matrix.keys()) for i in range(seq_length)]
+    max_score = sum(max_scores)
+
+    for i in range(len(promoter_sequence) - seq_length + 1):
+        seq = promoter_sequence[i:i + seq_length]
+        score = calculate_score(seq, matrix, max_scores)
+        normalized_score = score / max_score
+        results.append({'Sequence': seq, 'Score': normalized_score})
+
+        # Recherche dans le brin complémentaire
+        rev_seq = reverse_complement(seq)
+        rev_score = calculate_score(rev_seq, matrix, max_scores)
+        rev_normalized_score = rev_score / max_score
+        results.append({'Sequence': rev_seq, 'Score': rev_normalized_score})
+
+    # Affichage des résultats dans un tableau avec 3 chiffres significatifs
     table_data = []
     for result in results:
-        table_data.append([result['Sequence'], result['Score']])
+        table_data.append([result['Sequence'], f"{result['Score']:.3g}"])
 
     table = tabulate(table_data, headers=['Sequence', 'Score'], tablefmt='orgtbl')
     output_text.delete(1.0, tk.END)
     output_text.insert(tk.END, table)
+    max_score_label.config(text=f"Score maximum: {max_score:.3g}")
+    
+def reverse_complement(sequence):
+    complement_dict = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    rev_sequence = ''.join(complement_dict[base] for base in reversed(sequence))
+    return rev_sequence
 
-def calculate_score(sequence, matrix):
+def calculate_score(sequence, matrix, max_scores):
     score = 0
     for i, base in enumerate(sequence):
         if base in {'A', 'C', 'G', 'T'}:
-            base_scores = matrix[base]
-            score += base_scores[i]
+            base_score = matrix[base]
+            score += base_score[i]
     return score
-
 
 # Création de la fenêtre principale
 window = tk.Tk()
 window.title("Recherche de séquence Jaspar")
-window.geometry("400x300")
+window.geometry("400x350")
 
 # Frame pour les champs d'entrée et le bouton
 input_frame = ttk.Frame(window)
@@ -69,6 +86,10 @@ entry_promoter.grid(row=1, column=1, padx=5, pady=5)
 # Bouton de recherche
 search_button = ttk.Button(window, text="Rechercher", command=search_sequence)
 search_button.pack()
+
+# Étiquette pour afficher le score maximum
+max_score_label = ttk.Label(window, text="Score maximum: ")
+max_score_label.pack()
 
 # Zone de texte pour afficher les résultats
 output_text = tk.Text(window, width=40, height=10)
