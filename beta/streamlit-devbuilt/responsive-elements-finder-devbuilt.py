9df41f5dc9ec724cbe59d3bd841e6de15166b732
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-10 10:17:28 +0200

    Update responsive-elements-finder-devbuilt.py

--- beta/streamlit-devbuilt/responsive-elements-finder-devbuilt.py
+++ beta/streamlit-devbuilt/responsive-elements-finder-devbuilt.py
@@ -130,7 +130,7 @@ species_combobox = st.selectbox("Species:", ["Human", "Mouse", "Rat"], index=0)
 # Upstream
 upstream_entry = st.text_input("Upstream:", value="2000")
 updown_slide = st.slider("Up/down", -10000, 10000 , (-5000, 5000))
-st.write("up", min(updown_slide)
+st.write("up", -min(updown_slide))
 st.write("down", max(updown_slide))
 
 # Downstream
