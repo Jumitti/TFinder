272c225a56dfecd8fe894031823dbc7230244fbb
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-16 00:34:26 +0200

    Update responsive-elements-finder-devbuilt.py

--- beta/streamlit-devbuilt/responsive-elements-finder-devbuilt.py
+++ beta/streamlit-devbuilt/responsive-elements-finder-devbuilt.py
@@ -514,7 +514,8 @@ if jaspar:
             background_layer = alt.Chart(df).mark_image(
                 url=background_image_url,
                 width=600,
-                height=400
+                height=400,
+                aspect=False
                 
             ).properties(
                 width=600,
