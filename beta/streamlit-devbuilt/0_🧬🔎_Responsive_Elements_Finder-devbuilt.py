3b009ba7739bffbf525637bbec9e5a17a77eaa7f
Minniti Julien <minnitijulien06@gmail.com>
2023-06-21 10:45:15 +0200

    Update 0_🧬🔎_Responsive_Elements_Finder-devbuilt.py

--- beta/streamlit-devbuilt/0_🧬🔎_Responsive_Elements_Finder-devbuilt.py
+++ beta/streamlit-devbuilt/0_🧬🔎_Responsive_Elements_Finder-devbuilt.py
@@ -196,15 +196,13 @@ with col2:
     st.header(':red[Step 2] Binding Sites Finder')
     if prom_term == 'Promoter':
         if 'result_promoter' not in st.session_state:
-            st.info("⬇️ If :red[**Step 1**] not used, paste sequences here (FASTA required for multiple sequences).")
-            result_promoter = st.text_area("🔸 :red[**Step 2.1**] Promoter:", value="If :red[**Step 1**] not used, paste sequences here (FASTA required for multiple sequences).")
+            result_promoter = st.text_area("🔸 :red[**Step 2.1**] Promoter:", value="If Step 1 not used, paste sequences here (FASTA required for multiple sequences).")
         else:
             result_promoter_text = "\n".join(st.session_state['result_promoter'])
             result_promoter = st.text_area("🔸 :red[**Step 2.1**] Promoter:", value=result_promoter_text, help='Copy: Click in sequence, CTRL+A, CTRL+C')
     else:
         if 'result_promoter' not in st.session_state:
-            st.info("⬇️ If :red[**Step 1**] not used, paste sequences here (FASTA required for multiple sequences).")
-            result_promoter = st.text_area("🔸 :red[**Step 2.1**] Terminator:", value="If :red[**Step 1**] not used, paste sequences here (FASTA required for multiple sequences).")
+            result_promoter = st.text_area("🔸 :red[**Step 2.1**] Terminator:", value="If Step 1 not used, paste sequences here (FASTA required for multiple sequences).")
         else:
             result_promoter_text = "\n".join(st.session_state['result_promoter'])
             result_promoter = st.text_area("🔸 :red[**Step 2.1**] Terminator:", value=result_promoter_text, help='Copy: Click in sequence, CTRL+A, CTRL+C')
