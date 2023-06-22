240b9d861f57526c1e507261e81f3f8ecc47e1e8
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-22 21:20:56 +0200

    Minor fix

--- beta/streamlit-devbuilt/navigation/resource.py
+++ beta/streamlit-devbuilt/navigation/resource.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import requests
 
 def resource_page():
-    url = "https://github.com/Jumitti/Responsive-Elements-Finder/blob/main/Promoter_finder_HELP.pdf"
-    response = requests.get(url)
-
-    # Vérifier que la requête a réussi
-    if response.status_code == 200:
-        # Enregistrer le fichier PDF localement
-        with open("fichier.pdf", "wb") as f:
-            f.write(response.content)
     st.success('resource')
