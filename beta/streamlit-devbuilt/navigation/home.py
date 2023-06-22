6928248a79a2eeda6daa065a3e799b195acaa148
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-22 21:03:08 +0200

    Update home.py

--- beta/streamlit-devbuilt/navigation/home.py
+++ beta/streamlit-devbuilt/navigation/home.py
@@ -4,7 +4,7 @@ import datetime
 
 def home_page():
     
-    _, middle, _ = st.columns([0.1, 1, 0.1])
+    _, middle, _ = st.columns([1, 1, 1])
     
     with middle:
         st.header('Responsive Elements Finder 🧬🔎')
