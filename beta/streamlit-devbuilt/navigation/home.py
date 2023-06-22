a062d83c87c62a1d18e3a686bb38b6dab50ab58a
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-22 22:23:06 +0200

    Update home.py

--- beta/streamlit-devbuilt/navigation/home.py
+++ beta/streamlit-devbuilt/navigation/home.py
@@ -4,6 +4,8 @@ import datetime
 
 def home_page():
     st.header('Responsive Elements Finder 🧬🔎')
+    st.subheader('Schema')
+    st.text('schema')
     st.subheader('Abstract')
     st.text('Lorem ipsum')
     
