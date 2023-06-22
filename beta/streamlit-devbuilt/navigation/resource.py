a9885dabc88ed1a1a5ee6c0d45f19932ad5b31da
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-22 21:19:30 +0200

    add on

--- beta/streamlit-devbuilt/navigation/resource.py
+++ beta/streamlit-devbuilt/navigation/resource.py
@@ -1,6 +1,13 @@
 import streamlit as st
-from streamlit_pdf_viewer import pdf_viewer
+import requests
 
 def resource_page():
+    url = "https://github.com/Jumitti/Responsive-Elements-Finder/blob/main/Promoter_finder_HELP.pdf"
+    response = requests.get(url)
+
+    # Vérifier que la requête a réussi
+    if response.status_code == 200:
+        # Enregistrer le fichier PDF localement
+        with open("fichier.pdf", "wb") as f:
+            f.write(response.content)
     st.success('resource')
-    pdf_viewer("Promoter_finder_HELP.pdf")
