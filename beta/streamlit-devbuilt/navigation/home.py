234ffc72581f057e91132d48784ba503e1091498
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-22 21:00:26 +0200

    Update home.py

--- beta/streamlit-devbuilt/navigation/home.py
+++ beta/streamlit-devbuilt/navigation/home.py
@@ -4,8 +4,6 @@ import datetime
 
 TITLE = 'COVID-19db linkage maps of cell surface proteins and transcription factors in immune cells'
 
-
-
 def home_page():
     
     _, middle, _ = st.columns([0.1, 1, 0.1])
@@ -14,8 +12,5 @@ def home_page():
         # st.markdown('### ' + TITLE) 
         st.markdown(f"<h3 style='text-align: center; color: black;'>{TITLE}</h1>", unsafe_allow_html=True)  
         
-        st.markdown('👉 Read our paper here: https://www.biorxiv.org/content/10.1101/2022.12.14.520411v1')    
-        
-        
-        st.markdown(f"<p style='text-align: justify; color: black;'>{Desc.home}</h4>", unsafe_allow_html=True)  
+        st.markdown('👉 Read our paper here: https://www.biorxiv.org/content/10.1101/2022.12.14.520411v1')
     
