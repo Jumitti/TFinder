352688eba19fa05c57a704604973780e3ce86da7
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-23 21:33:24 +0200

    Update REF.py

--- beta/streamlit-devbuilt/navigation/REF.py
+++ beta/streamlit-devbuilt/navigation/REF.py
@@ -557,16 +557,6 @@ def REF_page():
 
                         matrix_array = np.array([matrix_data['A'], matrix_data['C'], matrix_data['G'], matrix_data['T']])
 
-                        # Define the transformation function
-                        def transform_matrix(matrix):
-                            transformed_matrix = {
-                                'A': [matrix[0][4], matrix[0][7], matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3], matrix[0][5], matrix[0][6], matrix[0][8]],
-                                'C': [matrix[1][3], matrix[1][7], matrix[1][8], matrix[1][1], matrix[1][0], matrix[1][2], matrix[1][4], matrix[1][5], matrix[1][6]],
-                                'G': [matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][7], matrix[2][6], matrix[2][8], matrix[2][3], matrix[2][4], matrix[2][5]],
-                                'T': [matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][5], matrix[3][7], matrix[3][8], matrix[3][3], matrix[3][4], matrix[3][6]]
-                            }
-                            return transformed_matrix
-
                         matrices = transform_matrix(matrix_array)
                             
                         table2 = search_sequence(threshold, tis_value, result_promoter, matrices)
