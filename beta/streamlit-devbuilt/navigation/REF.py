b0c08a2db5780b059f71d11671ea3359528552e6
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-23 23:23:57 +0200

    Update REF.py

--- beta/streamlit-devbuilt/navigation/REF.py
+++ beta/streamlit-devbuilt/navigation/REF.py
@@ -582,9 +582,10 @@ def REF_page():
 
                             matrix_output = st.text_area("PWM:", value=pwm_text, help='Select and copy for later use')
                 
-                    matrix_text = matrix_output
                         else:
                             st.warning("You forget FASTA sequences :)")
+                            
+                matrix_text = matrix_output
 
               
         else:
