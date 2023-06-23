c0d79b2529d0ca34ee4de4719a177d6247d042b5
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-23 23:09:38 +0200

    Update REF.py

--- beta/streamlit-devbuilt/navigation/REF.py
+++ beta/streamlit-devbuilt/navigation/REF.py
@@ -526,7 +526,7 @@ def REF_page():
                 matrix_text = st.text_area("🔸 :orange[**Step 2.3**] Matrix:", value="A [ 20.0 0.0 0.0 0.0 0.0 0.0 0.0 100.0 0.0 60.0 20.0 ]\nT [ 60.0 20.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 ]\nG [ 0.0 20.0 100.0 0.0 0.0 100.0 100.0 0.0 100.0 40.0 0.0 ]\nC [ 20.0 60.0 0.0 100.0 100.0 0.0 0.0 0.0 0.0 0.0 80.0 ]")
             else:
                 fasta_text = st.text_area("🔸 :orange[**Step 2.3**] Sequences:", value=">seq1\nCTGCCGGAGGA\n>seq2\nAGGCCGGAGGC\n>seq3\nTCGCCGGAGAC\n>seq4\nCCGCCGGAGCG\n>seq5\nAGGCCGGATCG", help='Put FASTA sequences. Same sequence length required ⚠️')
-                matrix_text = []
+                matrix_text = ""
                 def calculate_pwm(sequences):
                     num_sequences = len(sequences)
                     sequence_length = len(sequences[0])
