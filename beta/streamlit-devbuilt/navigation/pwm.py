9bd47ccd8f9d211c13215c66f84d9a3e02250e64
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-06-23 21:57:33 +0200

    Update pwm.py

--- beta/streamlit-devbuilt/navigation/pwm.py
+++ beta/streamlit-devbuilt/navigation/pwm.py
@@ -1,17 +1,20 @@
 import streamlit as st
-import numpy as np
+import ast
 import json
 
 def pwm_page():
     # Obtenir la matrice depuis la zone de texte
     matrix_text = st.text_area("Saisir la matrice")
 
-    # Convertir la chaîne de caractères en une structure de données Python
-    matrix = eval(matrix_text)
+    try:
+    matrix = ast.literal_eval(matrix_text)
+    except (SyntaxError, ValueError):
+        st.error("Erreur lors de l'analyse de la matrice. Assurez-vous qu'elle est correctement formatée.")
+        matrix = None
 
-    # Conversion au format JSON
-    json_matrix = json.dumps(matrix, indent=4)
+    if matrix:
+        # Conversion au format JSON
+        json_matrix = json.dumps(matrix, indent=4)
 
-    # Affichage du résultat
-    st.code(json_matrix, language='json')
-
+        # Affichage du résultat
+        st.code(json_matrix, language='json')
