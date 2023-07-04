4e4350cf1b6953b24651628d882453e6128e6b68
Minniti Julien <minnitijulien06@gmail.com>
2023-07-04 15:14:13 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -639,7 +639,7 @@ def aio_page():
                 source = df
                 score_range = source['Score %'].astype(float)
                 ystart = math.floor(score_range.min() - 0.2 )
-                st.write(ystart)
+                st.success(ystart)
                 ystop = math.floor(score_range.max() + (10/100))
                 scale = alt.Scale(scheme='category10')
                 color_scale = alt.Color("Promoter:N", scale=scale)
