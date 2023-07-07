fdf1e96458882f40de88658190ea92e3eca0ff5f
Minniti Julien <minnitijulien06@gmail.com>
2023-07-07 11:52:52 +0200

    Update BSF.py

--- beta/streamlit-devbuilt/navigation/BSF.py
+++ beta/streamlit-devbuilt/navigation/BSF.py
@@ -156,13 +156,13 @@ def BSF_page():
                         sequence_with_context = ''.join(sequence_parts)
                         tis_position = position - tis_value
 
-                        if normalized_score >= threshold:
-                            row = [str(position).ljust(8),
-                                   str(tis_position).ljust(15),
-                                   sequence_with_context,
-                                   "{:.3f}".format(normalized_score).ljust(12),"{:.3f}".format(weight).ljust(12),
-                                   shortened_promoter_name]
-                            table2.append(row)
+                        '''if normalized_score >= threshold:'''
+                        row = [str(position).ljust(8),
+                               str(tis_position).ljust(15),
+                               sequence_with_context,
+                               "{:.3f}".format(normalized_score).ljust(12),"{:.3f}".format(weight).ljust(12),
+                               shortened_promoter_name]
+                        table2.append(row)
 
         if len(table2) > 0:
             table2.sort(key=lambda x: float(x[3]), reverse=True)
