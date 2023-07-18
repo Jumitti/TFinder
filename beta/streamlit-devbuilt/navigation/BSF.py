c36a862b7e208f51b8aacd2731385db47f7de1e0
Minniti Julien <minnitijulien06@gmail.com>
2023-07-18 17:27:06 +0200

    Update BSF.py

--- beta/streamlit-devbuilt/navigation/BSF.py
+++ beta/streamlit-devbuilt/navigation/BSF.py
@@ -506,45 +506,49 @@ def BSF_page():
     if jaspar == 'JASPAR_ID':
         if 'table2' in locals():
             if len(table2) > 0:
+                st.divider()
                 jaspar_id = sequence_consensus_input
                 url = f"https://jaspar.genereg.net/api/v1/matrix/{jaspar_id}/"
                 response = requests.get(url)
                 response_data = response.json()
                 TF_name = response_data['name']
                 st.success(f"Finding responsive elements done for {TF_name}")
-                df = pd.DataFrame(table2[1:], columns=table2[0])
-                st.session_state['df'] = df
-                st.dataframe(df)
+                outcol1, outcol2 = st.columns(2)
+                with outcol1:
+                    df = pd.DataFrame(table2[1:], columns=table2[0])
+                    st.session_state['df'] = df
+                    st.dataframe(df)
 
-                csv = df.to_csv(index=False).encode('utf-8')
+                    csv = df.to_csv(index=False).encode('utf-8')
 
-                st.download_button("💾 Download (.csv)",csv,"file.csv","text/csv",key='download-csv')
-
-                source = df
-                score_range = source['Relative Score'].astype(float)
-                ystart = score_range.min() - 0.05
-                ystop = score_range.max() + 0.05
-                scale = alt.Scale(scheme='category10')
-                color_scale = alt.Color("Promoter:N", scale=scale)
-                
-                if calc_pvalue:
-                    chart = alt.Chart(source).mark_circle().encode(
-                        x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
-                        y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
-                        color=color_scale,
-                        tooltip=['Relative position', 'Relative Score', 'p-value', 'Sequence', 'Promoter']
-                    ).properties(width=600, height=400)
-                                          
-                    st.altair_chart(chart, use_container_width=True)
-                else:
-                    chart = alt.Chart(source).mark_circle().encode(
-                        x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
-                        y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
-                        color=color_scale,
-                        tooltip=['Relative position', 'Relative Score', 'Sequence', 'Promoter']
-                    ).properties(width=600, height=400)
-                                          
-                    st.altair_chart(chart, use_container_width=True)
+                    st.download_button("💾 Download (.csv)",csv,"file.csv","text/csv",key='download-csv')
+                    
+                with outcol2:
+                    source = df
+                    score_range = source['Relative Score'].astype(float)
+                    ystart = score_range.min() - 0.02
+                    ystop = score_range.max() + 0.02
+                    scale = alt.Scale(scheme='category10')
+                    color_scale = alt.Color("Promoter:N", scale=scale)
+                    
+                    if calc_pvalue:
+                        chart = alt.Chart(source).mark_circle().encode(
+                            x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
+                            y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
+                            color=color_scale,
+                            tooltip=['Relative position', 'Relative Score', 'p-value', 'Sequence', 'Promoter']
+                        ).properties(width=600, height=400)
+                                              
+                        st.altair_chart(chart, use_container_width=True)
+                    else:
+                        chart = alt.Chart(source).mark_circle().encode(
+                            x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
+                            y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
+                            color=color_scale,
+                            tooltip=['Relative position', 'Relative Score', 'Sequence', 'Promoter']
+                        ).properties(width=600, height=400)
+                                              
+                        st.altair_chart(chart, use_container_width=True)
             else: 
                 jaspar_id = sequence_consensus_input
                 url = f"https://jaspar.genereg.net/api/v1/matrix/{jaspar_id}/"
@@ -552,46 +556,47 @@ def BSF_page():
                 response_data = response.json()
                 TF_name = response_data['name']
                 st.error(f"No consensus sequence found with the specified threshold for {TF_name}")
-        else:
-            st.text("")
+                
     else:
         if 'table2' in locals():
             if len(table2) > 0:
+                st.divider()
                 st.success(f"Finding responsive elements done")
-                df = pd.DataFrame(table2[1:], columns=table2[0])
-                st.session_state['df'] = df
-                st.dataframe(df)
-                
-                csv = df.to_csv(index=False).encode('utf-8')
+                outcol1, outcol2 = st.columns(2)
+                with outcol1:
+                    df = pd.DataFrame(table2[1:], columns=table2[0])
+                    st.session_state['df'] = df
+                    st.dataframe(df)
+                    
+                    csv = df.to_csv(index=False).encode('utf-8')
 
-                st.download_button("💾 Download (.csv)",csv,"file.csv","text/csv",key='download-csv')
-
-                source = df
-                score_range = source['Relative Score'].astype(float)
-                ystart = score_range.min() - 0.05
-                ystop = score_range.max() + 0.05
-                scale = alt.Scale(scheme='category10')
-                color_scale = alt.Color("Promoter:N", scale=scale)
-                
-                if calc_pvalue:
-                    chart = alt.Chart(source).mark_circle().encode(
-                        x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
-                        y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
-                        color=color_scale,
-                        tooltip=['Relative position', 'Relative Score', 'p-value', 'Sequence', 'Promoter']
-                    ).properties(width=600, height=400)
-                                          
-                    st.altair_chart(chart, use_container_width=True)
-                else:
-                    chart = alt.Chart(source).mark_circle().encode(
-                        x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
-                        y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
-                        color=color_scale,
-                        tooltip=['Relative position', 'Relative Score', 'Sequence', 'Promoter']
-                    ).properties(width=600, height=400)
-                                          
-                    st.altair_chart(chart, use_container_width=True)
+                    st.download_button("💾 Download (.csv)",csv,"file.csv","text/csv",key='download-csv')
+                    
+                with outcol2:
+                    source = df
+                    score_range = source['Relative Score'].astype(float)
+                    ystart = score_range.min() - 0.02
+                    ystop = score_range.max() + 0.02
+                    scale = alt.Scale(scheme='category10')
+                    color_scale = alt.Color("Promoter:N", scale=scale)
+                    
+                    if calc_pvalue:
+                        chart = alt.Chart(source).mark_circle().encode(
+                            x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
+                            y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
+                            color=color_scale,
+                            tooltip=['Relative position', 'Relative Score', 'p-value', 'Sequence', 'Promoter']
+                        ).properties(width=600, height=400)
+                                              
+                        st.altair_chart(chart, use_container_width=True)
+                    else:
+                        chart = alt.Chart(source).mark_circle().encode(
+                            x=alt.X('Relative position:Q', axis=alt.Axis(title='Relative position (bp)'), sort='ascending'),
+                            y=alt.Y('Relative Score:Q', axis=alt.Axis(title='Relative Score'), scale=alt.Scale(domain=[ystart, ystop])),
+                            color=color_scale,
+                            tooltip=['Relative position', 'Relative Score', 'Sequence', 'Promoter']
+                        ).properties(width=600, height=400)
+                                              
+                        st.altair_chart(chart, use_container_width=True)
             else:
                 st.error(f"No consensus sequence found with the specified threshold")
-        else:
-            st.text("")
