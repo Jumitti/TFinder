5aec804b4a2b8c7d0b548b729ec13b9bc5aa97e3
Minniti Julien <minnitijulien06@gmail.com>
2023-07-18 15:52:34 +0200

    Update BSF.py

--- beta/streamlit-devbuilt/navigation/BSF.py
+++ beta/streamlit-devbuilt/navigation/BSF.py
@@ -103,6 +103,38 @@ def BSF_page():
                     i += 2
                 else:
                     i += 1
+        for promoter_region in promoters:
+            length_prom = len(promoter_region)
+            
+            if calc_pvalue :
+                def generate_random_sequence(length, probabilities):
+                    nucleotides = ['A', 'C', 'G', 'T']
+                    sequence = random.choices(nucleotides, probabilities, k=length)
+                    return ''.join(sequence)
+                 
+                random_scores = []
+                motif_length = seq_length
+                num_random_seqs = 500000
+                
+                count_a = promoter_region.count('A')
+                count_t = promoter_region.count('T')
+                count_g = promoter_region.count('G')
+                count_c = promoter_region.count('C')
+
+                percentage_a = count_a / length_prom
+                percentage_t = count_t / length_prom
+                percentage_g = count_g / length_prom
+                percentage_c = count_c / length_prom
+                
+                probabilities = [percentage_a, percentage_c, percentage_g, percentage_t]
+                
+                for _ in range(num_random_seqs):
+                    random_sequence = generate_random_sequence(motif_length, probabilities)
+                    random_score = calculate_score(random_sequence, matrix)
+                    normalized_random_score = (random_score - min_score)/(max_score - min_score)
+                    random_scores.append(normalized_random_score)
+
+                random_scores = np.array(random_scores)
         
         for matrix_name, matrix in matrices.items():
             seq_length = len(matrix['A'])
@@ -114,37 +146,12 @@ def BSF_page():
             # REF
             for shortened_promoter_name, promoter_region in promoters:
                 found_positions = []
-                length_prom = len(promoter_region)
+           
+                random_score = calculate_score(random_sequence, matrix)
+                normalized_random_score = (random_score - min_score)/(max_score - min_score)
+                random_scores.append(normalized_random_score)
 
-                if calc_pvalue :
-                    def generate_random_sequence(length, probabilities):
-                        nucleotides = ['A', 'C', 'G', 'T']
-                        sequence = random.choices(nucleotides, probabilities, k=length)
-                        return ''.join(sequence)
-                     
-                    random_scores = []
-                    motif_length = seq_length
-                    num_random_seqs = 500000
-                    
-                    count_a = promoter_region.count('A')
-                    count_t = promoter_region.count('T')
-                    count_g = promoter_region.count('G')
-                    count_c = promoter_region.count('C')
-
-                    percentage_a = count_a / length_prom
-                    percentage_t = count_t / length_prom
-                    percentage_g = count_g / length_prom
-                    percentage_c = count_c / length_prom
-                    
-                    probabilities = [percentage_a, percentage_c, percentage_g, percentage_t]
-                    
-                    for _ in range(num_random_seqs):
-                        random_sequence = generate_random_sequence(motif_length, probabilities)
-                        random_score = calculate_score(random_sequence, matrix)
-                        normalized_random_score = (random_score - min_score)/(max_score - min_score)
-                        random_scores.append(normalized_random_score)
-
-                    random_scores = np.array(random_scores)
+                random_scores = np.array(random_scores)
 
                 for i in range(len(promoter_region) - seq_length + 1):
                     seq = promoter_region[i:i + seq_length]
