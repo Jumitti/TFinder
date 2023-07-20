2843b21f402396e6e68f8170c3f234a9e3875610
Minniti Julien <minnitijulien06@gmail.com>
2023-07-20 16:53:41 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -45,6 +45,7 @@ def aio_page():
 
             # Request for ENTREZ_GENE_ID
             url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term={gene}[Gene%20Name]+AND+{species}[Organism]&retmode=json&rettype=xml"
+            st.success(url)
             response = requests.get(url)
 
             if response.status_code == 200:
