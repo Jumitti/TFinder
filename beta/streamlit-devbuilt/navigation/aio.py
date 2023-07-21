bfbc89d7f73b2c6def88c898f44128fd4489f6ef
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 16:22:47 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -248,7 +248,7 @@ def aio_page():
             
             st.write('**Auto complete species**')
             
-            species1, species2, species3, species4, species5 = st.columns(5)
+            species1, species2, species3, species4, species5 = st.columns([0.8,0.8,0.8,1.3,1.3])
             
             with species1:
                 all_human = st.checkbox("Human")
