b53a495a0cddcb76fdfdbd551a98aec18d46edb5
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 16:08:56 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -302,7 +302,7 @@ def aio_page():
                 data_df,
                 column_config={
                     "human": st.column_config.CheckboxColumn(
-                        "Human",
+                        "Human", help=None
                         default=False,
                     ),
                     "mouse": st.column_config.CheckboxColumn(
