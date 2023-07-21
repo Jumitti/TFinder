cfb24c8d0c1b9cdc5db1c71e478e5b0a06f7ae32
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 16:45:18 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -956,9 +956,9 @@ def aio_page():
                 st.success(f"Finding responsive elements done for {TF_name}")
                 outcol1, outcol2 = st.columns(2)
                 with outcol1:
-                    df = pd.DataFrame(table2[1:], columns=table2[0], hide_index=True)
+                    df = pd.DataFrame(table2[1:], columns=table2[0])
                     st.session_state['df'] = df
-                    st.dataframe(df)
+                    st.dataframe(df, index=False)
 
                     csv = df.to_csv(index=False).encode('utf-8')
 
