cea3af7cbff1833ffda5d3ddc556bb32d39f8575
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 16:31:02 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -332,7 +332,7 @@ def aio_page():
                         default=False,
                     )
                 },
-                disabled=["gene"],
+                disabled=["gene","human","mouse","rat","droso","zebra","prom"],
                 hide_index=True,
             )
             
