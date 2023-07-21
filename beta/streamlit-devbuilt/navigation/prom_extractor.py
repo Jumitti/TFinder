105a670c17bafdfb32dd1d7444e3430596d10046
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 14:57:15 +0200

    Update prom_extractor.py

--- beta/streamlit-devbuilt/navigation/prom_extractor.py
+++ beta/streamlit-devbuilt/navigation/prom_extractor.py
@@ -448,15 +448,8 @@ def prom_extractor_page():
         
         
     # Promoter output state
-    if prom_term == 'Promoter':
-        if 'result_promoter' not in st.session_state:
-            result_promoter = st.text_area("🔸 :red[**Step 1.6**] Promoter:", value="")
-        else:
-            result_promoter_text = "\n".join(st.session_state['result_promoter'])
-            result_promoter = st.text_area("🔸 :red[**Step 1.6**] Promoter:", value=result_promoter_text, help='Copy: Click in sequence, CTRL+A, CTRL+C')
+    if 'result_promoter' not in st.session_state:
+        result_promoter = st.text_area("🔸 :red[**Step 1.6**] Sequences:", value="")
     else:
-        if 'result_promoter' not in st.session_state:
-            result_promoter = st.text_area("🔸 :red[**Step 1.6**] Terminator:", value="")
-        else:
-            result_promoter_text = "\n".join(st.session_state['result_promoter'])
-            result_promoter = st.text_area("🔸 :red[**Step 1.6**] Terminator:", value=result_promoter_text, help='Copy: Click in sequence, CTRL+A, CTRL+C')        
+        result_promoter_text = "\n".join(st.session_state['result_promoter'])
+        result_promoter = st.text_area("🔸 :red[**Step 1.6**] Sequences:", value=result_promoter_text, help='Copy: Click in sequence, CTRL+A, CTRL+C')  
