064c79b69171806fb6918fd835e379a8b2cbaf47
Minniti Julien <minnitijulien06@gmail.com>
2023-07-21 16:34:46 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -156,10 +156,10 @@ def aio_page():
 
                 # Append the result to the result_promoter
                 if prom_term == 'Promoter':
-                    result_promoter.append(f">{gene_name} | {species_API} | {chraccver} | TSS (on chromosome): {chrstart}\n{dna_sequence}\n")
+                    result_promoter.append(f">{gene_name} | {species_API} | {chraccver} | {prom_term} | TSS (on chromosome): {chrstart}\n{dna_sequence}\n")
                     st.session_state['result_promoter'] = result_promoter
                 else:
-                    result_promoter.append(f">{gene_name} | {species_API} | {chraccver} | Gene end (on chromosome): {chrstop}\n{dna_sequence}\n")
+                    result_promoter.append(f">{gene_name} | {species_API} | {chraccver} | {prom_term} | Gene end (on chromosome): {chrstop}\n{dna_sequence}\n")
                     st.session_state['result_promoter'] = result_promoter
 
             return result_promoter
