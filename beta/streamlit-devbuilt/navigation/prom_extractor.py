8aa6c412599c942208873afec7bdad1d012be8ea
Minniti Julien <minnitijulien06@gmail.com>
2023-07-24 10:36:50 +0200

    Update prom_extractor.py

--- beta/streamlit-devbuilt/navigation/prom_extractor.py
+++ beta/streamlit-devbuilt/navigation/prom_extractor.py
@@ -169,6 +169,18 @@ def prom_extractor_page():
     result_promoter = []
     # Gene ID
     gene_id_entry = st.text_area("🔸 :red[**Step 1.1**] Gene ID:", value="PRKN\n5071")
+    for gene_input in gene_id_entry:
+        if not gene_input.isdigit():
+            species_list = ['human','mouse','rat','drosophila','zebrafish']
+            for species_test in species_list:
+                url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term={gene_input}[Gene%20Name]+AND+{species_test}[Organism]&retmode=json&rettype=xml"
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    response_data = response.json()
+
+                    if response_data['esearchresult']['count'] == '0':
+                        st.warning(f"No gene found for : {gene_input} for {species_test}", icon="⚠️")
     
     tab1, tab2 = st.tabs(['Default','Advance'])
     with tab1:
