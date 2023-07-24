03d931d8ab3fada33a1376d497e22853d58de095
Minniti Julien <minnitijulien06@gmail.com>
2023-07-24 14:01:26 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -334,7 +334,7 @@ def aio_page():
                 },
                 disabled=["Gene"],
                 hide_index=True,
-            )
+            ) 
             
             updown_slide = st.slider("🔸 :red[**Step 1.3**] Upstream/downstream from TSS and gene end (bp)", -10000, 10000, (-2000, 2000), step=100)
             st.write("Upstream: ", min(updown_slide), " bp from TSS and gene end | Downstream: ", max(updown_slide), " bp from TSS and gene end")
