3ea70969532e21a6caf8c135e1ecf6bc1312c9c9
Minniti Julien <minnitijulien06@gmail.com>
2023-07-28 14:50:02 +0200

    Update responsive_elements_finder-devbuilt.py

--- beta/streamlit-devbuilt/responsive_elements_finder-devbuilt.py
+++ beta/streamlit-devbuilt/responsive_elements_finder-devbuilt.py
@@ -215,7 +215,7 @@ def send_email(sender_email, sender_password, receiver_email, subject, body):
 # Interface utilisateur Streamlit
 st.title("Envoyer un e-mail avec Streamlit")
 
-sender_email = "noreply.results.tfinder@gmail.com"
+sender_email = "noreply.results.tfinder"
 sender_password = "PaulineJulien201097@" 
 receiver_email = st.text_input("Adresse e-mail du destinataire")
 subject = st.text_input("Sujet de l'e-mail")
