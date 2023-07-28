9f4fe3706ee18609a24ccfaac5ab2826206338ed
Minniti Julien <minnitijulien06@gmail.com>
2023-07-28 09:36:38 +0200

    Update components.py

--- beta/streamlit-devbuilt/utils/components.py
+++ beta/streamlit-devbuilt/utils/components.py
@@ -200,7 +200,7 @@ text-align: center;
 <p style="font-size: 13px">This software is distributed under an MIT licence. Please consult the LICENSE file for more details.</p>
 <a href="https://www.ipmc.cnrs.fr/cgi-bin/site.cgi"><img class="image2" src="https://www.ipmc.cnrs.fr/fichiers/images/logo_ipmc.jpg"alt="github" width="80" height=57"></a>
 <a href="https://www.cnrs.fr/fr"><img class="image2" src="https://www.cnrs.fr/themes/custom/cnrs/logo.svg"alt="github" width="70" height="70"></a>
-<a href="https://www.inserm.fr/"><img class="image2" src="https://www.sfr-biosciences.fr/medias/images/accueil/310842-logo-inserm.jpg"alt="github" width="70" height="70"></a>
+<a href="https://www.inserm.fr/"><img class="image2" src="https://www.sfr-biosciences.fr/medias/images/accueil/310842-logo-inserm.jpg"alt="github" width="90" height="70"></a>
 <a href="https://univ-cotedazur.fr/"><img class="image2" src="https://www6.inrae.fr/var/internet6_national_consortium_biocontrole/storage/images/les-membres/annuaire-des-membres-du-consortium-biocontrole/universite-cote-d-azur/39475-1-fre-FR/Universite-Cote-d-Azur_inra_image.png"alt="github" width="70" height="70"></a>
 </a><a href="https://github.com/Jumitti"><img class="image2" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" width="70" height="70"></a>
 """
