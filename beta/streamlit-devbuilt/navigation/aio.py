5874d6e9a5a8e0299577400ee15f056bc6d7de22
Minniti Julien <minnitijulien06@gmail.com>
2023-07-31 17:00:47 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -1145,6 +1145,9 @@ def aio_page():
                                 st.success('Email sent successfully! 🚀')
                         except Exception as e:
                             with colres5:
-                                st.error(f"Error sending e-mail : {e}") 
+                                if 'The recipient address <Send> is not a valid RFC-5321 address.' in e:
+                                    st.error(f"Invalid e-mail : {email_receiver}")
+                                else:
+                                    st.error(f"Error sending e-mail : {e}") 
             else:
                 st.error(f"No consensus sequence found with the specified threshold")
