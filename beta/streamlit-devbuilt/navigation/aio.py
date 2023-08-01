2b2e9f713044648ca4df83d602d04e5f62e4d3eb
Minniti Julien <minnitijulien06@gmail.com>
2023-08-01 10:02:05 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -728,8 +728,6 @@ def aio_page():
             with REcol2:
                 st.markdown("🔸 :orange[**Step 2.3**] Matrix:", help="Only PWM generated with our tools are allowed")
                 matrix_text = st.text_area("🔸 :orange[**Step 2.3**] Matrix:", value="A [ 20.0 0.0 0.0 0.0 0.0 0.0 0.0 100.0 0.0 60.0 20.0 ]\nT [ 60.0 20.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 ]\nG [ 0.0 20.0 100.0 0.0 0.0 100.0 100.0 0.0 100.0 40.0 0.0 ]\nC [ 20.0 60.0 0.0 100.0 100.0 0.0 0.0 0.0 0.0 0.0 80.0 ]", label_visibility='collapsed')
-                logo = logomaker.Logo(matrix_text, color_scheme = 'classic')
-                st.pyplot(logo.fig)
         else:
             with REcol1:
                 st.markdown("🔸 :orange[**Step 2.3**] Sequences:", help='Put FASTA sequences. Same sequence length required ⚠️')
@@ -1187,7 +1185,22 @@ def aio_page():
                 password = st.secrets['password']
                 attachment_excel = excel_file
                 attachment_text = txt_output
-                # attachment_png = 
+                
+                if jaspar == 'PWM':
+                    if matrix_type == 'With FASTA sequences':
+                        buffer = io.BytesIO()
+                        plt.savefig(buffer, format='png')
+                        buffer.seek(0)
+                        png_content = buffer.getvalue()
+                        attachment_png = png_content
+                elif jaspar == 'Manual sequence':
+                    buffer = io.BytesIO()
+                    plt.savefig(buffer, format='png')
+                    buffer.seek(0)
+                    png_content = buffer.getvalue()
+                    attachment_png = png_content
+                
+                
                 
                 with colres4:
                     if st.button("Send ✉"):
@@ -1209,16 +1222,24 @@ def aio_page():
                             attachment_text.add_header('Content-Disposition', 'attachment', filename=f'Sequences_{current_date_time}.txt')
                             msg.attach(attachment_text)
                             
-                            # attachment_png = MIMEBase('application', 'octet-stream')
-                            # attachment_png.set_payload(attachment_png)
-                            # attachment_png.add_header('Content-Disposition', 'attachment', filename='image.png')
-                            # msg.attach(attachment_png)
+                            if jaspar == 'PWM':
+                                if matrix_type == 'With FASTA sequences':
+                                    attachment_png = MIMEBase('application', 'octet-stream')
+                                    attachment_png.set_payload(attachment_png)
+                                    attachment_png.add_header('Content-Disposition', 'attachment', filename=f'logomaker_{current_date_time}.png')
+                                    msg.attach(attachment_png)
+                            elif jaspar == 'Manual sequence':
+                                attachment_png = MIMEBase('application', 'octet-stream')
+                                attachment_png.set_payload(attachment_png)
+                                attachment_png.add_header('Content-Disposition', 'attachment', filename=f'logomaker_{current_date_time}.png')
+                                msg.attach(attachment_png)
 
                             server = smtplib.SMTP('smtp.gmail.com', 587)
                             server.starttls()
                             server.login(email_sender, password)
                             server.sendmail(email_sender, email_receiver, msg.as_string())
                             server.quit()
+                            
                             with colres5:
                                 st.success('Email sent successfully! 🚀')
                         except smtplib.SMTPAuthenticationError:
