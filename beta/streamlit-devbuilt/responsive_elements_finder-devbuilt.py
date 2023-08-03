b557eaf76e0d4b87ed9b5935e383d4fa3881767a
Minniti Julien <minnitijulien06@gmail.com>
2023-08-03 14:47:34 +0200

    Update responsive_elements_finder-devbuilt.py

--- beta/streamlit-devbuilt/responsive_elements_finder-devbuilt.py
+++ beta/streamlit-devbuilt/responsive_elements_finder-devbuilt.py
@@ -181,7 +181,6 @@ st.sidebar.table(df)
 st.sidebar.markdown('✅: servers are reachable. ',help='You can use extract regions via NCBI/use the JASPAR_IDs')
 st.sidebar.markdown('❌: servers are unreachable. ',help='You can still use TFinder if you have a sequence in FASTA format and a pattern to search in the sequence')
 
-if st.sidebar.markdown("Report an issue/bug 🆘 -> [Click here](https://github.com/Jumitti/TFinder/issues/new/choose)")
+st.sidebar.markdown("Report an issue/bug 🆘 -> [Click here](https://github.com/Jumitti/TFinder/issues/new/choose)")
 
-if st.sidebar.markdown("Want to talk ? 🙋🏼‍♂️"):
-    webbrowser.open("https://github.com/Jumitti/TFinder/discussions")
+st.sidebar.markdown("Want to talk ? 🙋🏼‍♂️ -> [Chat Room](https://github.com/Jumitti/TFinder/discussions)")
