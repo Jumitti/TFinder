c58fee4957591bf846210c152076b2f599116326
Minniti Julien <minnitijulien06@gmail.com>
2023-08-03 15:48:41 +0200

    MC

--- beta/streamlit-devbuilt/navigation/allapp.py
+++ beta/streamlit-devbuilt/navigation/allapp.py
@@ -53,7 +53,7 @@ option_data = [
    
 ]
 
-over_theme = {'txc_inactive': 'black','menu_background':'#DEECFF','txc_active':'white','option_active':'#91C8E4'}
+over_theme = {'txc_inactive': 'black','menu_background':'#DEECFF','txc_active':'white','option_active':'#749BC2'}
 font_fmt = {'font-class':'h3','font-size':'50%'}
 
 def allapp_page():
