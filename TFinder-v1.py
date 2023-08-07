82c0855ae0f204a766469b549f953e6c30194b28
Minniti Julien <minnitijulien06@gmail.com>
2023-08-07 11:57:24 +0200

    Update TFinder-v1.py

--- TFinder-v1.py
+++ TFinder-v1.py
@@ -165,43 +165,22 @@ with st.sidebar.expander("Binding Sites Finder"):
     st.subheader('_p-value_')
     st.write('The p-value calculation takes time so it is optional. it represents the probability that a random generated sequence of the lenght of the PWM with the nucleotide proportions of the sequence has a score greater than or equal to the element found.')
 
-if ['ncbi_status','jaspar_status'] not in st.session_state:
+if st.sidebar.button("Check"):
     response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term=nos2[Gene%20Name]+AND+human[Organism]&retmode=json&rettype=xml')
     response1 = requests.get('https://jaspar.genereg.net/api/v1/matrix/MA0106.1')
 
     ncbi_status = "✅" if response.status_code == 200 else "❌"
     jaspar_status = "✅" if response1.status_code == 200 else "❌"
-    
-    st.session_state['ncbi_status'] = ncbi_status
-    st.session_state['jaspar_status'] = jaspar_status
 
     data = {
         "NCBI": [ncbi_status],
         "JASPAR": [jaspar_status]
     }
-
-    st.sidebar.title("Servers status", help='✅: servers are reachable. You can use extract regions via NCBI/use the JASPAR_IDs\n\n❌: servers are unreachable. You can still use TFinder if you have a sequence in FASTA format and a pattern to search in the sequence')
+    
     df = pd.DataFrame(data, index=["Servers status"])
 
     st.sidebar.table(df)
     
-    if st.sidebar.button("Refresh"):
-        del st.session_state['ncbi_status']
-        del st.session_state['jaspar_status']
-else:
-    data = {
-        "NCBI": [ncbi_status],
-        "JASPAR": [jaspar_status]
-    }
-    
-    st.sidebar.title("Servers status", help='✅: servers are reachable. You can use extract regions via NCBI/use the JASPAR_IDs\n\n❌: servers are unreachable. You can still use TFinder if you have a sequence in FASTA format and a pattern to search in the sequence')
-    df = pd.DataFrame(data, index=["Servers status"])
-
-    st.sidebar.table(df)
-    
-    if st.sidebar.button("Refresh"):
-        del st.session_state['ncbi_status']
-        del st.session_state['jaspar_status']
     
 
 st.sidebar.title("More")
