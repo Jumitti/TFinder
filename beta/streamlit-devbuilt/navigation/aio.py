d34e605aece2f000b8d82a810880aee3ed04a102
Minniti Julien <minnitijulien06@gmail.com>
2023-08-11 14:50:53 +0200

    Update aio.py

--- beta/streamlit-devbuilt/navigation/aio.py
+++ beta/streamlit-devbuilt/navigation/aio.py
@@ -167,6 +167,8 @@ def aio_page():
                 species_API = gene_info['organism']['scientificname']
 
                 dna_sequence = get_dna_sequence(chraccver, chrstart, chrstop, upstream, downstream)
+                
+                st.toast('Your edited image was saved!', icon='😍')
 
                 # Append the result to the result_promoter
                 if prom_term == 'Promoter':
