868f37209e6d4ddbf4fee53e44ae7fbd3448360e
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-13 17:38:05 +0200

    Update aio.py

--- navigation/aio.py
+++ navigation/aio.py
@@ -1049,6 +1049,51 @@ def aio_page():
     
     st.divider()
     # RE output
+    def email(excel_file, current_date_time, attachment_text, txt_output, email_receiver):
+        try:
+            email_sender = st.secrets['sender']
+            password = st.secrets['password']
+            
+            msg = MIMEMultipart()
+            msg['From'] = email_sender
+            msg['To'] = email_receiver
+            msg['Subject'] = subject
+
+            msg.attach(MIMEText(body, 'plain'))
+
+            attachment_excel = MIMEBase('application', 'octet-stream')
+            attachment_excel.set_payload(excel_file.getvalue())
+            encoders.encode_base64(attachment_excel)
+            attachment_excel.add_header('Content-Disposition', 'attachment', filename=f'Results_TFinder_{current_date_time}.xlsx')
+            msg.attach(attachment_excel)
+
+            attachment_text = MIMEText(txt_output, 'plain', 'utf-8')
+            attachment_text.add_header('Content-Disposition', 'attachment', filename=f'Sequences_{current_date_time}.txt')
+            msg.attach(attachment_text)
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(email_sender, password)
+            server.sendmail(email_sender, email_receiver, msg.as_string())
+            server.quit()
+            st.toast('Email sent successfully!', icon='🚀')
+            
+        except smtplib.SMTPAuthenticationError:
+            with colres5:
+                st.toast("Failed to authenticate. Please check your email and password.")
+        except smtplib.SMTPServerDisconnected:
+            with colres5:
+                st.toast("Failed to connect to the SMTP server. Please check your internet connection.")
+        except smtplib.SMTPRecipientsRefused:
+            with colres5:
+                st.toast(f"Error sending email: {email_receiver}")
+        except smtplib.SMTPException as e:
+            with colres5:
+                st.toast(f"Error sending email: {e}")
+        except Exception as e:
+            with colres5:
+                st.toast(f"Unknown error occurred: {e}")
+        
     if jaspar == 'JASPAR_ID':
         if 'table2' in locals():
             if len(table2) > 0:
@@ -1105,57 +1150,14 @@ def aio_page():
                     st.markdown('**Graph**',help='Zoom +/- with the mouse wheel. Drag while pressing the mouse to move the graph. Selection of a group by clicking on a point of the graph (double click de-selection). Double-click on a point to reset the zoom and the moving of graph.')
                     st.altair_chart(chart, theme=None, use_container_width=True)
                     
-                email_sender = st.secrets['sender']
                 with colres4:
                     email_receiver = st.text_input('Send results by email ✉', value='Send results by email ✉', label_visibility='collapsed')
                 subject = f'Results TFinder - {current_date_time}'
                 body = f"Hello ☺\n\nResults obtained with TFinder.\n\nJASPAR_ID: {jaspar_id} | Transcription Factor name: {TF_name}\n\nRelScore Threshold:\n{threshold_entry}\n\nThis email also includes the sequences used in FASTA format and an Excel table of results.\n\nFor all requests/information, please refer to the 'Contact' tab on the TFinder website. We would be happy to answer all your questions.\n\nBest regards\nTFinder Team\n\n\n\nN.B: Sometimes the WebLogo is not sent correctly. A small bug that I did not have time to fix (soon...). You can always right click 'Save Image' on the WebLogo on TFinder directly."
-                password = st.secrets['password']
-                attachment_excel = excel_file
-                attachment_text = txt_output
                 
                 with colres4:
                     if st.button("Send ✉"):
-                        try:
-                            msg = MIMEMultipart()
-                            msg['From'] = email_sender
-                            msg['To'] = email_receiver
-                            msg['Subject'] = subject
-
-                            msg.attach(MIMEText(body, 'plain'))
-
-                            attachment_excel = MIMEBase('application', 'octet-stream')
-                            attachment_excel.set_payload(excel_file.getvalue())
-                            encoders.encode_base64(attachment_excel)
-                            attachment_excel.add_header('Content-Disposition', 'attachment', filename=f'Results_TFinder_{current_date_time}.xlsx')
-                            msg.attach(attachment_excel)
-
-                            attachment_text = MIMEText(attachment_text, 'plain', 'utf-8')
-                            attachment_text.add_header('Content-Disposition', 'attachment', filename=f'Sequences_{current_date_time}.txt')
-                            msg.attach(attachment_text)
-
-                            server = smtplib.SMTP('smtp.gmail.com', 587)
-                            server.starttls()
-                            server.login(email_sender, password)
-                            server.sendmail(email_sender, email_receiver, msg.as_string())
-                            server.quit()
-                            with colres5:
-                                st.success('Email sent successfully! 🚀')
-                        except smtplib.SMTPAuthenticationError:
-                            with colres5:
-                                st.error("Failed to authenticate. Please check your email and password.")
-                        except smtplib.SMTPServerDisconnected:
-                            with colres5:
-                                st.error("Failed to connect to the SMTP server. Please check your internet connection.")
-                        except smtplib.SMTPRecipientsRefused:
-                            with colres5:
-                                st.error(f"Error sending email: {email_receiver}")
-                        except smtplib.SMTPException as e:
-                            with colres5:
-                                st.error(f"Error sending email: {e}")
-                        except Exception as e:
-                            with colres5:
-                                st.error(f"Unknown error occurred: {e}")
+                        email(excel_file, current_date_time, attachment_text, txt_output, email_receiver)
                                 
             else: 
                 jaspar_id = sequence_consensus_input
