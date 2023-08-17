e9574e46b9a93eaad66547bf92c5a38c494457e3
Minniti Julien <minnitijulien06@gmail.com>
2023-08-17 10:17:11 +0200

    Cleaning code + fix extraction when a gene doesn't exist for a species

--- navigation/aio.py
+++ navigation/aio.py
@@ -157,7 +157,9 @@ def aio_page():
                     entrez_id = gene_id
                 else:
                     entrez_id = convert_gene_to_entrez_id(gene_id, species)
-                    if gene_id == 'not_found':
+                    if gene_id != 'not_found':
+                        pass
+                    else:
                         break
 
                 gene_info = get_gene_info(entrez_id)
