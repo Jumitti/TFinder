7e74b77327ab138a4c7f8db624aec72ed331c5c7
Minniti Julien <minnitijulien06@gmail.com>
2023-08-18 10:09:45 +0200

    Cleaning code + minor fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -412,7 +412,7 @@ def aio_page():
                                     prom_term = search_type.capitalize()
                                     species = 'human'  # This is just a remnant of the past
                                     try:
-                                        result_promoter = find_promoters(gene_ids, species, upstream, downstream)
+                                        result_promoter.extend(find_promoters(gene_ids, species, upstream, downstream))
                                     except Exception as e:
                                         st.error(f"Error finding {gene_ids}: {str(e)}")
                         else:
@@ -421,7 +421,7 @@ def aio_page():
                                     if getattr(gene_info, f'{species}') and getattr(gene_info, f'{search_type}'):
                                         prom_term = search_type.capitalize()
                                         try:
-                                            result_promoter = find_promoters(gene_ids, species, upstream, downstream)
+                                            result_promoter.extend(find_promoters(gene_ids, species, upstream, downstream))
                                         except Exception as e:
                                             st.error(f"Error finding {gene_ids}: {str(e)}")
 
