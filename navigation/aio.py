d2908ee990caf22dc6252fb94ec51d72eb7519f6
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-18 13:34:47 +0200

    Cleaning code + minor fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -176,13 +176,12 @@ def aio_page():
                 if prom_term == 'Promoter':
                     result_promoter.append(
                         f">{gene_name} | {species_API} | {chraccver} | {prom_term} | TSS (on chromosome): {chrstart}\n{dna_sequence}\n")
-                    result_promoter_text = "\n".join(result_promoter)
-                    st.session_state['result_promoter'] = result_promoter_text
                 else:
                     result_promoter.append(
                         f">{gene_name} | {species_API} | {chraccver} | {prom_term} | Gene end (on chromosome): {chrstop}\n{dna_sequence}\n")
-                    result_promoter_text = "\n".join(result_promoter)
-                    st.session_state['result_promoter'] = result_promoter_text
+
+                result_promoter_text = "\n".join(result_promoter)
+                st.session_state['result_promoter'] = result_promoter_text
 
             return result_promoter
 
@@ -536,14 +535,14 @@ def aio_page():
                     words = promoter_name.lstrip('>').split()
                     shortened_promoter_name = words[0]
                     for species in species_prom:
-                        if species in promoter_name:
+                        if species.lower() in promoter_name.lower():
                             found_species = species
                             break
                         else:
                             found_species = "n.d"
                     regions_prom = ['Promoter', 'Terminator']
                     for regions in regions_prom:
-                        if regions in promoter_name:
+                        if regions.lower() in promoter_name.lower():
                             region = regions
                             break
                         else:
