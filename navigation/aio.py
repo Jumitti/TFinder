86cf2c6d69d9415f21f782bf913fddba2e791444
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-21 17:04:28 +0200

    Cleaning code + minor fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -1003,7 +1003,14 @@ def aio_page():
             current_date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
             st.subheader(':blue[Results]')
 
+            df = pd.DataFrame(table2[1:], columns=table2[0])
+            st.session_state['df'] = df
+            st.markdown('**Table**')
+
             tablecol1, tablecol2 = st.columns([0.75, 0.25])
+            with tablecol1:
+                st.dataframe(df, hide_index=True)
+
             with tablecol2:
                 st.success(f"Finding responsive elements done !")
                 if jaspar == 'PWM':
@@ -1015,12 +1022,6 @@ def aio_page():
                     body = f"Hello 🧬\n\nResults obtained with TFinder.\n\nJASPAR_ID: {sequence_consensus_input} | Transcription Factor name: {TF_name}\n\nRelScore Threshold:\n{threshold_entry}\n\nThis email also includes the sequences used in FASTA format and an Excel table of results.\n\nFor all requests/information, please refer to the 'Contact' tab on the TFinder website. We would be happy to answer all your questions.\n\nBest regards\nTFinder Team 🔎🧬"
                 else:
                     body = f"Hello 🧬\n\nResults obtained with TFinder.\n\nResponsive Elements:\n{IUPAC}\n\nPosition Weight Matrix:\n{matrix_text}\n\nRelScore Threshold:\n{threshold_entry}\n\nThis email also includes the sequences used in FASTA format and an Excel table of results.\n\nFor all requests/information, please refer to the 'Contact' tab on the TFinder website. We would be happy to answer all your questions.\n\nBest regards\nTFinder Team 🔎🧬"
-
-            df = pd.DataFrame(table2[1:], columns=table2[0])
-            st.session_state['df'] = df
-            st.markdown('**Table**')
-            with tablecol1:
-                st.dataframe(df, hide_index=True)
 
             excel_file = io.BytesIO()
             df.to_excel(excel_file, index=False, sheet_name='Sheet1')
@@ -1039,6 +1040,6 @@ def aio_page():
                 email_receiver = st.text_input('Send results by email ✉', value='Send results by email ✉',
                                                label_visibility='collapsed')
                 if st.button("Send ✉"):
-                    email(excel_file, txt_output, email_receiver, body)
+                    email(excel_file, email_receiver, body)
         else:
             st.error(f"No consensus sequence found with the specified threshold")
