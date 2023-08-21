3f5756c3a084f29762c3e88cbe80ad30adb88287
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-21 09:59:25 +0200

    Cleaning code + minor fix Error when PWM dont have same lenght of sequences

--- navigation/aio.py
+++ navigation/aio.py
@@ -846,7 +846,7 @@ def aio_page():
             try:
                 matrix_text, buffer = im(fasta_text)
             except Exception as e:
-                st.error({str(e)})
+                st.error(e)
 
     else:
         with REcol1:
@@ -867,7 +867,7 @@ def aio_page():
             try:
                 matrix_text, buffer = im(fasta_text)
             except Exception as e:
-                st.error({str(e)})
+                st.error(e)
 
         else:
             isUIPAC = False
