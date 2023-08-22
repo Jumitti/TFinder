23540723beaa09fb851d1be0b68464e2b0fc8d39
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-22 11:10:32 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -210,182 +210,184 @@ def aio_page():
         tab1, tab2 = st.tabs(['Default', 'Advance'])
 
         with tab1:
+            with st.form("Default"):
 
-            # Species
-            st.markdown("🔹 :blue[**Step 1.2**] Select species of gene names:")
-            species = st.selectbox("🔹 :blue[**Step 1.2**] Select species of gene names:",
-                                   ["Human", "Mouse", "Rat", "Drosophila", "Zebrafish"], index=0,
-                                   label_visibility='collapsed')
+                # Species
+                st.markdown("🔹 :blue[**Step 1.2**] Select species of gene names:")
+                species = st.selectbox("🔹 :blue[**Step 1.2**] Select species of gene names:",
+                                       ["Human", "Mouse", "Rat", "Drosophila", "Zebrafish"], index=0,
+                                       label_visibility='collapsed')
 
-            # Upstream/Downstream Promoter
-            st.markdown("🔹 :blue[**Step 1.3**] Regulatory region:")
-            prom_term = st.radio("🔹 :blue[**Step 1.3**] Regulatory region:", ('Promoter', 'Terminator'),
-                                 label_visibility='collapsed')
-            if prom_term == 'Promoter':
-                st.markdown("🔹 :blue[**Step 1.4**] Upstream/downstream from the TSS (bp)")
-            else:
-                st.markdown("🔹 :blue[**Step 1.4**] Upstream/downstream from gene end (bp)")
+                # Upstream/Downstream Promoter
+                st.markdown("🔹 :blue[**Step 1.3**] Regulatory region:")
+                prom_term = st.radio("🔹 :blue[**Step 1.3**] Regulatory region:", ('Promoter', 'Terminator'),
+                                     label_visibility='collapsed')
+                if prom_term == 'Promoter':
+                    st.markdown("🔹 :blue[**Step 1.4**] Upstream/downstream from the TSS (bp)")
+                else:
+                    st.markdown("🔹 :blue[**Step 1.4**] Upstream/downstream from gene end (bp)")
 
-            updown_slide = st.slider("🔹 :blue[**Step 1.4**] Upstream/downstream", -10000, 10000,
-                                     (-2000, 2000), step=100, label_visibility='collapsed')
-            if prom_term == 'Promoter':
-                st.write("Upstream: ", min(updown_slide), " bp from TSS | Downstream: ", max(updown_slide),
-                         " bp from TSS")
-            else:
-                st.write("Upstream: ", min(updown_slide), " bp from gene end | Downstream: ", max(updown_slide),
-                         " bp from gene end")
+                updown_slide = st.slider("🔹 :blue[**Step 1.4**] Upstream/downstream", -10000, 10000,
+                                         (-2000, 2000), step=100, label_visibility='collapsed')
+                if prom_term == 'Promoter':
+                    st.write("Upstream: ", min(updown_slide), " bp from TSS | Downstream: ", max(updown_slide),
+                             " bp from TSS")
+                else:
+                    st.write("Upstream: ", min(updown_slide), " bp from gene end | Downstream: ", max(updown_slide),
+                             " bp from gene end")
 
-            upstream_entry = -min(updown_slide)
-            downstream_entry = max(updown_slide)
+                upstream_entry = -min(updown_slide)
+                downstream_entry = max(updown_slide)
 
-            # Run Promoter Finder
-            if st.button(f"🧬 :blue[**Step 1.5**] Extract {prom_term}", help='(~5sec/gene)'):
-                with colprom1:
-                    with st.spinner("Finding promoters..."):
-                        gene_ids = gene_id_entry.strip().split("\n")
-                        upstream = int(upstream_entry)
-                        st.session_state['upstream'] = upstream
-                        downstream = int(downstream_entry)
-                        try:
-                            result_promoter = find_promoters(gene_ids, species, upstream, downstream)
-                            st.success(f"{prom_term} extraction complete !")
-                            st.toast(f"{prom_term} extraction complete !", icon='😊')
-                        except Exception as e:
-                            st.error(f"Error finding {prom_term}: {str(e)}")
+                # Run Promoter Finder
+                if st.button(f"🧬 :blue[**Step 1.5**] Extract {prom_term}", help='(~5sec/gene)'):
+                    with colprom1:
+                        with st.spinner("Finding promoters..."):
+                            gene_ids = gene_id_entry.strip().split("\n")
+                            upstream = int(upstream_entry)
+                            st.session_state['upstream'] = upstream
+                            downstream = int(downstream_entry)
+                            try:
+                                result_promoter = find_promoters(gene_ids, species, upstream, downstream)
+                                st.success(f"{prom_term} extraction complete !")
+                                st.toast(f"{prom_term} extraction complete !", icon='😊')
+                            except Exception as e:
+                                st.error(f"Error finding {prom_term}: {str(e)}")
 
         with tab2:
-            # Advance mode extraction
-            data_df = pd.DataFrame(
-                {
-                    "Gene": gene_list,
-                    "human": [False] * len(gene_list),
-                    "mouse": [False] * len(gene_list),
-                    "rat": [False] * len(gene_list),
-                    "drosophila": [False] * len(gene_list),
-                    "zebrafish": [False] * len(gene_list),
-                    "promoter": [False] * len(gene_list),
-                    "terminator": [False] * len(gene_list),
-                }
-            )
+            with st.form("Advance"):
+                # Advance mode extraction
+                data_df = pd.DataFrame(
+                    {
+                        "Gene": gene_list,
+                        "human": [False] * len(gene_list),
+                        "mouse": [False] * len(gene_list),
+                        "rat": [False] * len(gene_list),
+                        "drosophila": [False] * len(gene_list),
+                        "zebrafish": [False] * len(gene_list),
+                        "promoter": [False] * len(gene_list),
+                        "terminator": [False] * len(gene_list),
+                    }
+                )
 
-            species_list = ['human', 'mouse', 'rat', 'drosophila', 'zebrafish']
-            search_types = ['promoter', 'terminator']
+                species_list = ['human', 'mouse', 'rat', 'drosophila', 'zebrafish']
+                search_types = ['promoter', 'terminator']
 
-            st.markdown('**🔹 :blue[Step 1.2]** Select species for all genes:',
-                        help='Checking a box allows you to check all the corresponding boxes for each gene. Warning: if you have manually checked boxes in the table, they will be reset.')
+                st.markdown('**🔹 :blue[Step 1.2]** Select species for all genes:',
+                            help='Checking a box allows you to check all the corresponding boxes for each gene. Warning: if you have manually checked boxes in the table, they will be reset.')
 
-            species1, species2, species3, species4, species5 = st.columns(5)
+                species1, species2, species3, species4, species5 = st.columns(5)
 
-            with species1:
-                all_human = st.checkbox("Human")
-            with species2:
-                all_mouse = st.checkbox("Mouse")
-            with species3:
-                all_rat = st.checkbox("Rat")
-            with species4:
-                all_droso = st.checkbox("Drosophila")
-            with species5:
-                all_zebra = st.checkbox("Zebrafish")
+                with species1:
+                    all_human = st.checkbox("Human")
+                with species2:
+                    all_mouse = st.checkbox("Mouse")
+                with species3:
+                    all_rat = st.checkbox("Rat")
+                with species4:
+                    all_droso = st.checkbox("Drosophila")
+                with species5:
+                    all_zebra = st.checkbox("Zebrafish")
 
-            st.markdown('**🔹 :blue[Step 1.2]** Select regions for all genes:',
-                        help='Checking a box allows you to check all the corresponding boxes for each gene. Warning: if you have manually checked boxes in the table, they will be reset.')
+                st.markdown('**🔹 :blue[Step 1.2]** Select regions for all genes:',
+                            help='Checking a box allows you to check all the corresponding boxes for each gene. Warning: if you have manually checked boxes in the table, they will be reset.')
 
-            region1, region2 = st.columns(2)
+                region1, region2 = st.columns(2)
 
-            with region1:
-                all_prom = st.checkbox("Promoter")
-            with region2:
-                all_term = st.checkbox("Terminator")
+                with region1:
+                    all_prom = st.checkbox("Promoter")
+                with region2:
+                    all_term = st.checkbox("Terminator")
 
-            if all_human:
-                data_df["human"] = True
-            if all_mouse:
-                data_df["mouse"] = True
-            if all_rat:
-                data_df["rat"] = True
-            if all_droso:
-                data_df["drosophila"] = True
-            if all_zebra:
-                data_df["zebrafish"] = True
-            if all_prom:
-                data_df["promoter"] = True
-            if all_term:
-                data_df["terminator"] = True
+                if all_human:
+                    data_df["human"] = True
+                if all_mouse:
+                    data_df["mouse"] = True
+                if all_rat:
+                    data_df["rat"] = True
+                if all_droso:
+                    data_df["drosophila"] = True
+                if all_zebra:
+                    data_df["zebrafish"] = True
+                if all_prom:
+                    data_df["promoter"] = True
+                if all_term:
+                    data_df["terminator"] = True
 
-            st.markdown('**🔹 :blue[Step 1.2]** On demand genes table',
-                        help="Check the boxes for which you want to extract a sequence. Pay attention that the gene name is equivalent for each species. The choice of species is not available for gene IDs. Parameterize the table last, if you check the boxes above, it resets the whole table.")
+                st.markdown('**🔹 :blue[Step 1.2]** On demand genes table',
+                            help="Check the boxes for which you want to extract a sequence. Pay attention that the gene name is equivalent for each species. The choice of species is not available for gene IDs. Parameterize the table last, if you check the boxes above, it resets the whole table.")
 
-            data_dff = st.data_editor(
-                data_df,
-                column_config={
-                    "human": st.column_config.CheckboxColumn(
-                        "Human",
-                        default=False,
-                    ),
-                    "mouse": st.column_config.CheckboxColumn(
-                        "Mouse",
-                        default=False,
-                    ),
-                    "rat": st.column_config.CheckboxColumn(
-                        "Rat",
-                        default=False,
-                    ),
-                    "drosophila": st.column_config.CheckboxColumn(
-                        "Drosophila",
-                        default=False,
-                    ),
-                    "zebrafish": st.column_config.CheckboxColumn(
-                        "Zebrafish",
-                        default=False,
-                    ),
-                    "promoter": st.column_config.CheckboxColumn(
-                        "Promoter",
-                        default=False,
-                    ),
-                    "terminator": st.column_config.CheckboxColumn(
-                        "Terminator",
-                        default=False,
-                    )
-                },
-                disabled=["Gene"],
-                hide_index=True,
-            )
+                data_dff = st.data_editor(
+                    data_df,
+                    column_config={
+                        "human": st.column_config.CheckboxColumn(
+                            "Human",
+                            default=False,
+                        ),
+                        "mouse": st.column_config.CheckboxColumn(
+                            "Mouse",
+                            default=False,
+                        ),
+                        "rat": st.column_config.CheckboxColumn(
+                            "Rat",
+                            default=False,
+                        ),
+                        "drosophila": st.column_config.CheckboxColumn(
+                            "Drosophila",
+                            default=False,
+                        ),
+                        "zebrafish": st.column_config.CheckboxColumn(
+                            "Zebrafish",
+                            default=False,
+                        ),
+                        "promoter": st.column_config.CheckboxColumn(
+                            "Promoter",
+                            default=False,
+                        ),
+                        "terminator": st.column_config.CheckboxColumn(
+                            "Terminator",
+                            default=False,
+                        )
+                    },
+                    disabled=["Gene"],
+                    hide_index=True,
+                )
 
-            updown_slide = st.slider("🔹 :blue[**Step 1.3**] Upstream/downstream from TSS and gene end (bp)", -10000,
-                                     10000, (-2000, 2000), step=100, label_visibility='collapsed')
-            st.write("Upstream: ", min(updown_slide), " bp from TSS and gene end | Downstream: ", max(updown_slide),
-                     " bp from TSS and gene end")
-            upstream_entry = -min(updown_slide)
-            downstream_entry = max(updown_slide)
+                updown_slide = st.slider("🔹 :blue[**Step 1.3**] Upstream/downstream from TSS and gene end (bp)", -10000,
+                                         10000, (-2000, 2000), step=100, label_visibility='collapsed')
+                st.write("Upstream: ", min(updown_slide), " bp from TSS and gene end | Downstream: ", max(updown_slide),
+                         " bp from TSS and gene end")
+                upstream_entry = -min(updown_slide)
+                downstream_entry = max(updown_slide)
 
-            if st.button("🧬 :blue[**Step 1.4**] Extract sequences", help="(~5sec/seq)"):
-                with colprom1:
-                    with st.spinner("Finding sequences..."):
-                        st.session_state['upstream'] = upstream_entry
-                        upstream = int(upstream_entry)
-                        downstream = int(downstream_entry)
-                        for gene_info in data_dff.itertuples(index=False):
-                            gene_name = gene_info.Gene
-                            gene_ids = gene_name.strip().split('\n')
-                            if gene_name.isdigit():
-                                for search_type in search_types:
-                                    if getattr(gene_info, f'{search_type}'):
-                                        prom_term = search_type.capitalize()
-                                        species = 'human'  # This is just a remnant of the past
-                                        try:
-                                            result_promoter = find_promoters(gene_ids, species, upstream, downstream)
-                                        except Exception as e:
-                                            st.error(f"Error finding {gene_ids}: {str(e)}")
-                            else:
-                                for species in species_list:
+                if st.button("🧬 :blue[**Step 1.4**] Extract sequences", help="(~5sec/seq)"):
+                    with colprom1:
+                        with st.spinner("Finding sequences..."):
+                            st.session_state['upstream'] = upstream_entry
+                            upstream = int(upstream_entry)
+                            downstream = int(downstream_entry)
+                            for gene_info in data_dff.itertuples(index=False):
+                                gene_name = gene_info.Gene
+                                gene_ids = gene_name.strip().split('\n')
+                                if gene_name.isdigit():
                                     for search_type in search_types:
-                                        if getattr(gene_info, f'{species}') and getattr(gene_info, f'{search_type}'):
+                                        if getattr(gene_info, f'{search_type}'):
                                             prom_term = search_type.capitalize()
+                                            species = 'human'  # This is just a remnant of the past
                                             try:
-                                                result_promoter = find_promoters(gene_ids, species, upstream,
-                                                                                 downstream)
+                                                result_promoter = find_promoters(gene_ids, species, upstream, downstream)
                                             except Exception as e:
                                                 st.error(f"Error finding {gene_ids}: {str(e)}")
+                                else:
+                                    for species in species_list:
+                                        for search_type in search_types:
+                                            if getattr(gene_info, f'{species}') and getattr(gene_info, f'{search_type}'):
+                                                prom_term = search_type.capitalize()
+                                                try:
+                                                    result_promoter = find_promoters(gene_ids, species, upstream,
+                                                                                     downstream)
+                                                except Exception as e:
+                                                    st.error(f"Error finding {gene_ids}: {str(e)}")
 
     # Promoter output state
     st.divider()
