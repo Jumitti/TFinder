e7222dfd2ac67c0e056708dfe5232be63a6093f6
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-23 11:20:12 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -272,7 +272,7 @@ def aio_page():
 
         total_iterations = len(matrices.items())*len(total_promoter_region_length)
 
-        with tqdm(total=total_iterations, desc='Calculating scores', mininterval=1) as pbar:
+        with stqdm(total=total_iterations, desc='Calculating scores', mininterval=1) as pbar:
 
             if calc_pvalue:
                 for matrix_name, matrix in matrices.items():
