2426ed59545b4ae1de281a3835c25b3f50dbe83d
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-23 12:25:22 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -267,10 +267,10 @@ def aio_page():
                     i += 1
 
         total_promoter_region_length = sum(len(promoter_region) for _, promoter_region, _, _ in promoters)
-        if calc_pvalue:
+        '''if calc_pvalue:
             total_iterations = (len(matrices.items()) * (total_promoter_region_length + len(promoter_region)*1000000))
-        else:
-            total_iterations = len(matrices.items()) * total_promoter_region_length
+        else:'''
+        total_iterations = len(matrices.items()) * total_promoter_region_length
 
         with stqdm(total=total_iterations, desc='Calculating scores', mininterval=0.1) as pbar:
 
