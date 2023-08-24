672a71ec9c220318854b92db8b0f17b460cba352
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-24 15:59:23 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -834,15 +834,11 @@ def aio_page():
     with promcol1:
         st.markdown("🔹 :blue[**Step 2.1**] Sequences:", help='Copy: Click in sequence, CTRL+A, CTRL+C')
         if not 'result_promoter_text' in st.session_state:
-            result_promoter = st.text_area("🔹 :blue[**Step 2.1**] Sequences:",
-                                       value='',
+            result_promoter_text = ''
+        result_promoter = st.text_area("🔹 :blue[**Step 2.1**] Sequences:",
+                                       value=result_promoter_text,
                                        placeholder='If Step 1 not used, paste sequences here (FASTA required for multiple sequences).',
                                        label_visibility='collapsed')
-        else:
-            result_promoter = st.text_area("🔹 :blue[**Step 2.1**] Sequences:",
-                                           value=st.session_state['result_promoter_text'],
-                                           placeholder='If Step 1 not used, paste sequences here (FASTA required for multiple sequences).',
-                                           label_visibility='collapsed')
 
     with promcol2:
         st.markdown('')
