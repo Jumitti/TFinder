7a3d64f7cef5a2a1523c3814ffdaf36d5550f39e
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-24 23:02:51 +0200

    Fix and improvements

--- navigation/aio.py
+++ navigation/aio.py
@@ -826,28 +826,30 @@ def aio_page():
                                                                                     f'{search_type}'):
                                         iterration += 1
                     st.write(iterration)
-                    for gene_info in (data_dff.itertuples(index=False)):
-                        gene_name = gene_info.Gene
-                        gene_ids = gene_name.strip().split('\n')
-                        if gene_name.isdigit():
-                            for search_type in search_types:
-                                if getattr(gene_info, f'{search_type}'):
-                                    prom_term = search_type.capitalize()
-                                    species = 'human'  # This is just a remnant of the past
-                                    try:
-                                        result_promoter = find_promoters(gene_ids, species, upstream,downstream)
-                                    except Exception as e:
-                                        st.error(f"Error finding {gene_ids}: {str(e)}")
-                        else:
-                            for species in species_list:
+                    with stqdm(value=iterration) as pbar:
+                        for gene_info in (data_dff.itertuples(index=False)):
+                            pbar.update(1)
+                            gene_name = gene_info.Gene
+                            gene_ids = gene_name.strip().split('\n')
+                            if gene_name.isdigit():
                                 for search_type in search_types:
-                                    if getattr(gene_info, f'{species}') and getattr(gene_info,
-                                                                                    f'{search_type}'):
+                                    if getattr(gene_info, f'{search_type}'):
                                         prom_term = search_type.capitalize()
+                                        species = 'human'  # This is just a remnant of the past
                                         try:
                                             result_promoter = find_promoters(gene_ids, species, upstream,downstream)
                                         except Exception as e:
                                             st.error(f"Error finding {gene_ids}: {str(e)}")
+                            else:
+                                for species in species_list:
+                                    for search_type in search_types:
+                                        if getattr(gene_info, f'{species}') and getattr(gene_info,
+                                                                                        f'{search_type}'):
+                                            prom_term = search_type.capitalize()
+                                            try:
+                                                result_promoter = find_promoters(gene_ids, species, upstream,downstream)
+                                            except Exception as e:
+                                                st.error(f"Error finding {gene_ids}: {str(e)}")
 
                         result_promoter_text = "\n".join(result_promoter)
                         st.session_state['result_promoter_text'] = result_promoter_text
