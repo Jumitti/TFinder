a33bea17eeeca1b1da806ee1aad4ea516e807f67
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-24 14:57:27 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -673,11 +673,12 @@ def aio_page():
             if st.button(f"🧬 :blue[**Step 1.5**] Extract {prom_term}", help='(~5sec/gene)', key='Default'):
                 with colprom1:
                     with st.spinner("Finding promoters..."):
-                        if 'result_promoter' in st.session_state:
+                        if 'result_promoter_text' in st.session_state:
                             del st.session_state['result_promoter']
                         try:
                             result_promoter = find_promoters(gene_ids, species, upstream, downstream)
-                            st.session_state['result_promoter'] = result_promoter
+                            result_promoter_text = "\n".join(result_promoter)
+                            st.session_state['result_promoter_text'] = result_promoter_text
                             st.success(f"{prom_term} extraction complete !")
                             st.toast(f"{prom_term} extraction complete !", icon='😊')
                         except Exception as e:
@@ -832,15 +833,14 @@ def aio_page():
     promcol1, promcol2 = st.columns([0.9, 0.1], gap='small')
     with promcol1:
         st.markdown("🔹 :blue[**Step 2.1**] Sequences:", help='Copy: Click in sequence, CTRL+A, CTRL+C')
-        if 'result_promoter' in st.session_state:
-            result_promoter_text = "\n".join(st.session_state['result_promoter'])
-            update = ''
-        else:
+        if not 'result_promoter_text' in st.session_state:
             result_promoter_text = ''
+            st.session_state['result_promoter_text']
         result_promoter = st.text_area("🔹 :blue[**Step 2.1**] Sequences:",
-                                       value=result_promoter_text,
+                                       value=st.session_state['result_promoter_text'],
                                        placeholder='If Step 1 not used, paste sequences here (FASTA required for multiple sequences).',
                                        label_visibility='collapsed')
+
     with promcol2:
         st.markdown('')
         st.markdown('')
