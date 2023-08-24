8d45e638cf8353b2c8f6959f1ce86fb6d3fffe72
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-24 09:49:49 +0200

    Cleaning code + fix

--- navigation/aio.py
+++ navigation/aio.py
@@ -1032,14 +1032,12 @@ def aio_page():
                     matrix[key.strip()] = values
             button = False
     st.markdown("")
-    runBSF = False
-    if st.button("🔹 :blue[**Step 2.6**] Click here to find motif in your sequences 🔎 🧬", use_container_width=True, disabled=button) and runBSF == False:
-        if result_promoter.startswith(("A", "T", "G", "C", ">", "a", "t", "c", "g", "n")):
-            runBSF = True
-            matrices = transform_matrix(matrix)
-            table2 = search_sequence(threshold, tis_value, promoters, matrices, total_promoter_region_length)
-            st.session_state['table2'] = table2
-            runBSF = False
+    with st.form("runBSF"):
+        if st.form_submit_button("🔹 :blue[**Step 2.6**] Click here to find motif in your sequences 🔎 🧬", use_container_width=True, disabled=button):
+            if result_promoter.startswith(("A", "T", "G", "C", ">", "a", "t", "c", "g", "n")):
+                matrices = transform_matrix(matrix)
+                table2 = search_sequence(threshold, tis_value, promoters, matrices, total_promoter_region_length)
+                st.session_state['table2'] = table2
 
     st.divider()
     if 'table2' in st.session_state:
