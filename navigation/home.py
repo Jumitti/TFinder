35919b18b4ec16651f3e3939ad8440b72a494aaf
Minniti Julien <minnitijulien06@gmail.com>
2023-08-25 14:44:03 +0200

    Cleaning code + minor fix

--- navigation/home.py
+++ navigation/home.py
@@ -30,6 +30,7 @@ def home_page():
         unsafe_allow_html=True)
     with open('./clock.time', 'r') as f:
         last_updated_on = f.readlines()[0]
+    st.caption(last_updated_on)
     st.markdown('')
     st.image('https://raw.githubusercontent.com/Jumitti/TFinder/Beta/img/tfinder_schema_bg_V5-3.png',
              caption='Just a test')
