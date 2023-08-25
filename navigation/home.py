907243b1d4ef9769e9ccf714b994abf2eb290901
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-25 18:58:54 +0200

    Add schema

--- navigation/home.py
+++ navigation/home.py
@@ -32,7 +32,7 @@ def home_page():
         last_updated_on = f.readlines()[0]
     st.caption(last_updated_on)
     st.markdown('')
-    st.image('https://raw.githubusercontent.com/Jumitti/TFinder/Beta/img/tfinder_schema_bg_V5-3.png',
+    st.image('./img/tfinder_schema.png',
              caption='Just a test')
     st.markdown('**Overview**')
     st.markdown(
