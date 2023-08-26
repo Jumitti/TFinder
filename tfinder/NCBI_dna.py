f52afb011664c3d67ceb5ec71357b759b0c0ad02
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-26 19:05:36 +0200

    Class...

---
+++ tfinder/NCBI_dna.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2023 Minniti Julien
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of TFinder and associated documentation files, to deal
+# in TFinder without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of TFinder, and to permit persons to whom TFinder is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of TFinder.
+
+# TFINDER IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH TFINDER OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class NCBI_dna:
+    species_list = ['Human', 'Mouse', 'Rat', 'Drosophila', 'Zebrafish']
+    def __init__(self):
+        self.gene_name = gene_name
+
+    def reverse_complement(sequence):
+        complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+        reverse_sequence = sequence[::-1]
+        complement_sequence = ''.join(complement_dict.get(base, base) for base in reverse_sequence)
+        return complement_sequence
+
+    @classmethod
+    def analyse_gene(cls, gene_id):
+        disponibility_list = ['ID', 'Human', 'Mouse', 'Rat', 'Drosophila', 'Zebrafish']
+        time.sleep(0.25)
+        gene_analyse = [gene_id]
+        for species_test in disponibility_list:
+            if not gene_id.isdigit():
+                if species_test == 'ID':
+                    gene_analyse.append('n.d')
+                else:
+                    time.sleep(0.5)
+                    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term={gene_id}[Gene%20Name]+AND+{species_test}[Organism]&retmode=json&rettype=xml"
+                    response = requests.get(url)
+
+                    if response.status_code == 200:
+                        response_data = response.json()
+
+                        if response_data['esearchresult']['count'] != '0':
+                            gene_analyse.append("✅")
+                        else:
+                            gene_analyse.append("❌")
+
+            if gene_id.isdigit():
+                if species_test != 'ID':
+                    gene_analyse.append('n.d')
+                else:
+                    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id={gene_id}&retmode=json&rettype=xml"
+                    response = requests.get(url)
+
+                    if response.status_code == 200:
+                        response_data = response.json()
+
+                        if 'chraccver' in str(response_data):
+                            gene_analyse.append("✅")
+                        else:
+                            gene_analyse.append("❌")
+
+        return gene_analyse
