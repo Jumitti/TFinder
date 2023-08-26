400cb2e3f5a6ba2ea0241dba0bc21bce5534fb80
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-26 21:19:51 +0200

    Class...

--- navigation/aio.py
+++ navigation/aio.py
@@ -805,11 +805,8 @@ def aio_page():
                     downstream = int(downstream_entry)
                     iterration = 0
                     for gene_info in (data_dff.itertuples(index=False)):
-                        gene_name = gene_info.Gene
-                        st.write(gene_name)
-                        gene_ids = gene_name.strip().split('\n')
-                        st.write(gene_ids)
-                        if gene_name.isdigit():
+                        gene_id = gene_info.Gene
+                        if gene_id.isdigit():
                             for search_type in search_types:
                                 if getattr(gene_info, f'{search_type}'):
                                     iterration += 1
@@ -823,17 +820,16 @@ def aio_page():
                                desc='**:blue[Extract sequence...] ⚠️:red[PLEASE WAIT UNTIL END WITHOUT CHANGING ANYTHING]**',
                                mininterval=0.1) as pbar:
                         for gene_info in (data_dff.itertuples(index=False)):
-                            gene_name = gene_info.Gene
-                            gene_ids = gene_name.strip().split('\n')
-                            if gene_name.isdigit():
+                            gene_id = gene_info.Gene
+                            if gene_id.isdigit():
                                 for search_type in search_types:
                                     if getattr(gene_info, f'{search_type}'):
                                         prom_term = search_type.capitalize()
                                         species = 'human'  # This is just a remnant of the past
                                         try:
-                                            result_promoter = find_promoters(gene_ids, species, upstream, downstream, prom_term)
+                                            result_promoter = find_promoters(gene_id, species, upstream, downstream, prom_term)
                                         except Exception as e:
-                                            st.error(f"Error finding {gene_ids}: {str(e)}")
+                                            st.error(f"Error finding {gene_id}: {str(e)}")
                                         pbar.update(1)
                             else:
                                 for species in species_list:
@@ -841,10 +837,10 @@ def aio_page():
                                         if getattr(gene_info, f'{species}') and getattr(gene_info, f'{search_type}'):
                                             prom_term = search_type.capitalize()
                                             try:
-                                                result_promoter = find_promoters(gene_ids, species, upstream,
+                                                result_promoter = find_promoters(gene_id, species, upstream,
                                                                                  downstream, prom_term)
                                             except Exception as e:
-                                                st.error(f"Error finding {gene_ids}: {str(e)}")
+                                                st.error(f"Error finding {gene_id}: {str(e)}")
                                             pbar.update(1)
 
                         result_promoter_text = "\n".join(result_promoter)
