55af03aa6e423d5b3bf37f1b67982501cb734fcd
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-08-26 21:04:44 +0200

    Class...

--- navigation/aio.py
+++ navigation/aio.py
@@ -804,45 +804,43 @@ def aio_page():
                     upstream = int(upstream_entry)
                     downstream = int(downstream_entry)
                     iterration = 0
-                    for gene_info in (data_dff.itertuples(index=False)):
-                        gene_name = gene_info.Gene
-                        gene_ids = gene_name.strip().split('\n')
+                    for gene_id in (data_dff.itertuples(index=False)):
+                        gene_name = gene_id.Gene
                         if gene_name.isdigit():
                             for search_type in search_types:
-                                if getattr(gene_info, f'{search_type}'):
+                                if getattr(gene_id, f'{search_type}'):
                                     iterration += 1
                         else:
                             for species in species_list:
                                 for search_type in search_types:
-                                    if getattr(gene_info, f'{species}') and getattr(gene_info,
+                                    if getattr(gene_id, f'{species}') and getattr(gene_info,
                                                                                     f'{search_type}'):
                                         iterration += 1
                     with stqdm(total=iterration,
                                desc='**:blue[Extract sequence...] ⚠️:red[PLEASE WAIT UNTIL END WITHOUT CHANGING ANYTHING]**',
                                mininterval=0.1) as pbar:
-                        for gene_info in (data_dff.itertuples(index=False)):
-                            gene_name = gene_info.Gene
-                            gene_ids = gene_name.strip().split('\n')
+                        for gene_id in (data_dff.itertuples(index=False)):
+                            gene_name = gene_id.Gene
                             if gene_name.isdigit():
                                 for search_type in search_types:
-                                    if getattr(gene_info, f'{search_type}'):
+                                    if getattr(gene_id, f'{search_type}'):
                                         prom_term = search_type.capitalize()
                                         species = 'human'  # This is just a remnant of the past
                                         try:
-                                            result_promoter = find_promoters(gene_ids, species, upstream, downstream)
+                                            result_promoter = find_promoters(gene_id, species, upstream, downstream)
                                         except Exception as e:
-                                            st.error(f"Error finding {gene_ids}: {str(e)}")
+                                            st.error(f"Error finding {gene_id}: {str(e)}")
                                         pbar.update(1)
                             else:
                                 for species in species_list:
                                     for search_type in search_types:
-                                        if getattr(gene_info, f'{species}') and getattr(gene_info, f'{search_type}'):
+                                        if getattr(gene_id, f'{species}') and getattr(gene_id, f'{search_type}'):
                                             prom_term = search_type.capitalize()
                                             try:
-                                                result_promoter = find_promoters(gene_ids, species, upstream,
+                                                result_promoter = find_promoters(gene_id, species, upstream,
                                                                                  downstream)
                                             except Exception as e:
-                                                st.error(f"Error finding {gene_ids}: {str(e)}")
+                                                st.error(f"Error finding {gene_id}: {str(e)}")
                                             pbar.update(1)
 
                         result_promoter_text = "\n".join(result_promoter)
