ab00e532d427f1da25f8416a679faa752f7c2222
Minniti Julien <minnitijulien06@gmail.com>
2023-09-01 09:49:17 +0200

    Cleaning code + minor fix

--- navigation/resource.py
+++ navigation/resource.py
@@ -28,7 +28,7 @@ def resource_page():
     st.markdown('')
     st.markdown('<div style="text-align: justify;"><p style="text-indent: 2em;">TFinder is a Python easy-to-use web tool for the identification of putative Transcription Factor Binding Sites (TFBS) in a sequence. It allows the extraction of the promoter or terminal regions of an unlimited number of genes via the NCBI API of up to five different species. The reference pattern (ex: a TFBS) accepts both IUPAC codes and JASPAR entries. It is also possible to generate and to use a Position Weight Matrix (PWM). Finally, the data may be recovered in either a tabular or graphic formats, showing the relevance score of the TFBSs found as a function of their relative position in the sequence. In this document, we will detail each part of TFinder, the methodology used and the resulting advantages and limitations of the software. TFinder is composed of two main modules that are structured in different sub-modules necessary for its functioning. We will not go into the specific details of the underlying code but will explain the principles and processes. We will first describe how to retrieve a nucleotide sequence on NCBI, then how to find a specific pattern in a nucleotide sequence.</p></div>', unsafe_allow_html=True)
     st.divider()
-    st.markdown("<h3 style='text-align: center; color: black;'>Extraction of gene regulatory sequences (promoter/terminator) 🧬</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: black;'>Regulatory Regions Extractor (promoter/terminator) 🧬</h1>", unsafe_allow_html=True)
     st.markdown('')
     st.markdown(f'<div style="text-align: justify;"><p style="text-indent: 2em;">{TEST}</p></div>', unsafe_allow_html=True)
     st.markdown("<div style='text-align: justify;'><p style='text-indent: 2em;'>Transcription Factors are proteins that bind to DNA to regulate gene expression. They specifically recognize a nucleotide sequence called a Transcription Factor Binding Site present in the 5’ end (most of the time proximal and core promoter regions) and sometimes 3’ end of the regulatory sequences of a gene (terminator regions) (<strong>Fig.4</strong>). TFinder allows the extraction of these 2 regions. NCBI does not allow direct extraction of regions external to genes. You have to do an extraction directly on the chromosome. We therefore need to know on which chromosome is located the gene of interest, where it begins and ends. The API makes it possible to recover this information as well as GeneBank of the gene (see <strong>Fig.5</strong>). With the API we can find the start and end coordinates of the gene on the chromosome. By having these coordinates, it is possible to know the meaning. Chromosomal coordinates are in the 5` to 3` direction. This means that the first nucleotide is 1 and the last 100000 (for example). If a gene starts at chromosomal coordinates 200 and ends at 500, it is in the 5`-> 3` direction. If a gene starts at 500 but ends at 200, it's the other way around. This makes it possible to transform the extracted region always in the 5` to 3` direction of the gene. After obtaining the start and end coordinates, we can choose to extract the upstream/downstream information by setting the corresponding window (<strong>Fig.4</strong>). Thanks to the ACCESION of the chromosome by setting the coordinates calculated with the upstream/downstream button, one can extract the requested region.</p></div>", unsafe_allow_html=True) 
@@ -54,7 +54,7 @@ def resource_page():
         st.code('>PRKN | Homo sapiens | NC_000006.12 | Promoter | TSS (on chromosome): 162727765\nATGAATACAGGTTTAGGAAAAAACAGAAAAGAACCCCAACCAGTAAAAAAAAAATTAAAGTATAACATTAAAAAACATCAAAATTGTAAATATTGTGTAGAAGAAAAACTAAATGATTAACCTGAATGGTTATGGTATTGCTGATAAATGCATCATCTTGA\n\n>APP | Homo sapiens | NC_000021.9 | Terminaotr | Gene end (on chromosome): 26171127\nACGCCATTCTCCTGCCTCAGCCTCCCCAGTAGCTGGGACTACAGGCGCCCGCCACGACGCCCGGCTAATTTTTTGTATTTTTAGTAGAGACGGGGTTTCACCGTGTTAGCCAGGATGGTGTTGATCTCCTGACCTCGTGATCCGCCCGCCTCAGCCTCCCAA')
         
     st.divider()
-    st.markdown("<h3 style='text-align: center; color: black;'>Transcription Factors Binding Site 🔎</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: black;'>Individual Motif Finder 🔎</h1>", unsafe_allow_html=True)
     st.markdown('')
     st.markdown('<div style="text-align: justify;"><p style="text-indent: 2em;">TFinder allows you to search for specific patterns in the desired sequences. The sequences must be put in <strong>Step 2.1</strong> (<strong>Fig.1</strong>). The FASTA format is authorized for multiple sequences.', unsafe_allow_html=True)
     st.markdown('<div style="text-align: justify;"><p style="text-indent: 2em;">The pattern to search for can be added in <strong>Step 2.2</strong>. A manual pattern can be used (<strong>Fig.1</strong>). TFinder supports the IUPAC format for your specific pattern (<strong>Fig.1</strong> Step 2.3 and <strong>Fig.2</strong>). A Position Weight Matrix (PWM) is generated to be reused by the user if necessary, as well as a weblogo. You can also use a JASPAR_ID (<strong>Fig.3</strong> and <strong>Fig.4</strong>) or use a PWM (<strong>Fig.5</strong>). If you decide to use a PWM, you can create it from sequences in FASTA format of the same length, or use a PWM generated with our tool.</p></div>', unsafe_allow_html=True) 
@@ -73,4 +73,35 @@ def resource_page():
         st.image('https://github.com/Jumitti/TFinder/blob/main/img/JASPAR%20ID.png?raw=true', caption='Figure 4: JASPAR_ID')
     with ttab5:
         st.image('https://github.com/Jumitti/TFinder/blob/main/img/bsfM.png?raw=true', caption='Figure 5: Screenshot of TFinder Binding Site Finder tools (option PWM)')
+
+    st.divider()
+    st.markdown("<h3 style='text-align: center; color: black;'>References 🔎</h1>", unsafe_allow_html=True)
+    st.markdown("Bailey TL, Johnson J, Grant CE et al. The MEME Suite. Nucleic Acids Res 2015;43:W39–49.")
+    st.markdown(
+        "Castro-Mondragon JA, Riudavets-Puig R, Rauluseviciute I et al. JASPAR 2022: the 9th release of the open-access database of transcription factor binding profiles. Nucleic Acids Research 2022;50:D165–73.")
+    st.markdown(
+        "Farre D. Identification of patterns in biological sequences at the ALGGEN server: PROMO and MALGEN. Nucleic Acids Research 2003;31:3651–3.")
+    st.markdown(
+        "Grant CE, Bailey TL, Noble WS. FIMO: scanning for occurrences of a given motif. Bioinformatics 2011;27:1017–8.")
+    st.markdown(
+        "Jayaram N, Usvyat D, R. Martin AC. Evaluating tools for transcription factor binding site prediction. BMC Bioinformatics 2016;17:547.")
+    st.markdown(
+        "Khan A, Fornes O, Stigliani A et al. JASPAR 2018: update of the open-access database of transcription factor binding profiles and its web framework. Nucleic Acids Research 2018;46:D260–6.")
+    st.markdown(
+        "Khan A, Mathelier A. JASPAR RESTful API: accessing JASPAR data from any programming language. Wren J (ed.). Bioinformatics 2018;34:1612–4.")
+    st.markdown(
+        "Matys V. TRANSFAC(R) and its module TRANSCompel(R): transcriptional gene regulation in eukaryotes. Nucleic Acids Research 2006;34:D108–10.")
+    st.markdown(
+        "Sayers EW, Bolton EE, Brister JR et al. Database resources of the national center for biotechnology information. Nucleic Acids Research 2022;50:D20–6.")
+    st.markdown(
+        "Tareen A, Kinney JB. Logomaker: beautiful sequence logos in Python. Valencia A (ed.). Bioinformatics 2020;36:2272–4.")
+    st.markdown(
+        "Touzet H, Varré J-S. Efficient and accurate P-value computation for Position Weight Matrices. Algorithms Mol Biol 2007;2:15.")
+    st.markdown(
+        "Tsunoda T, Takagi T. Estimating transcription factor bindability on DNA. Bioinformatics 1999;15:622–30.")
+    st.markdown(
+        "Turatsinze J-V, Thomas-Chollier M, Defrance M et al. Using RSAT to scan genome sequences for transcription factor binding sites and cis-regulatory modules. Nat Protoc 2008;3:1578–88.")
+    st.markdown(
+        "Wasserman WW, Sandelin A. Applied bioinformatics for the identification of regulatory elements. Nat Rev Genet 2004;5:276–87.")
+
     
