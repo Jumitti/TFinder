f36ba4b22d7d9c44238a16300cf15a602b755746
Minniti Julien <minnitijulien06@gmail.com>
2023-09-12 12:35:55 +0200

    Documentation API

--- navigation/tfinder_api.py
+++ navigation/tfinder_api.py
@@ -42,6 +42,6 @@ def tfinder_api():
         st.markdown('(list)')
         st.divider()
         st.markdown('**Examples**')
-        example = 'for gene_id in gene_ids: ' \
-                  'NCBIdna(gene_id).analyse_gene())'
+        example = '''for gene_id in gene_ids:
+                    NCBIdna(gene_id).analyse_gene())'''
         st.code(example)
