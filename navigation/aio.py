5a8a071863d9e152561f0b972bf3737ddfc627ef
Minniti Julien <minnitijulien06@gmail.com>
2023-10-16 11:23:42 +0200

    Fix all variants with slice variant input

--- navigation/aio.py
+++ navigation/aio.py
@@ -260,7 +260,8 @@ def aio_page():
                                 st.error(result_promoter_output)
                                 continue
 
-                        if all_variants:
+                        if all_variants and not gene_id.startswith('XM_') or not gene_id.startswith(
+                                'NM_') or not gene_id.startswith('XR_') or not gene_id.startswith('NR_'):
                             result_promoter_text = result_promoter_output
                         else:
                             result_promoter_text = "\n".join(result_promoter)
